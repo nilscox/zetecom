9b1497cb4bbf0bc6b97d1a862bc62a5d6620f2f9
Nils Layet <nils@epita.fr>
2018-11-16 19:28:22 +0100

    add production configuration

--- config/config.js
+++ config/config.js
@@ -1,7 +1,12 @@
-module.exports.development = {
+const config = {
   host: process.env.CDV_DB_HOST,
   username: process.env.CDV_DB_USER,
   password: process.env.CDV_DB_PASSWORD,
   database: process.env.CDV_DB_NAME,
   dialect: 'postgres',
+};
+
+module.exports = {
+  development: config,
+  production: config,
 };
