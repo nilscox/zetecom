d508ca7caa6d614ce897fefaa3495fea8cd3ffe6
Nils Layet <nils@epita.fr>
2018-11-20 13:35:44 +0100

    code style

--- webapp/src/components/Reaction/ReactionMessage.js
+++ webapp/src/components/Reaction/ReactionMessage.js
@@ -3,16 +3,22 @@ import Linkify from 'react-linkify';
 
 const ReactionMessage = ({ message }) => (
   <div className="reaction-message p-2">
-    {message.split('\n\n').map((p, n) => (
+
+    { message.split('\n\n').map((p, n) => (
       <p key={n}>
-        {p.split('\n').map((l, n) => (
+
+        { p.split('\n').map((l, n) => (
           <span key={n}>
-            <Linkify properties={{ target: '_blank' }}>{l}</Linkify>
+            <Linkify properties={{ target: '_blank' }}>
+              { l }
+            </Linkify>
             <br />
           </span>
-        ))}
+        )) }
+
       </p>
-    ))}
+    )) }
+
   </div>
 );
 
