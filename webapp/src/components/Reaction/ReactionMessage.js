b4bb206dcae7004a7c6faf0ceff09685db986cdf
Bopzor <violaine.ballandin@gmail.com>
2018-11-20 13:24:53 +0100

    add the reaction message component

---
+++ webapp/src/components/Reaction/ReactionMessage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Linkify from 'react-linkify';
+
+const ReactionMessage = ({ message }) => (
+  <div className="reaction-message p-2">
+    {message.split('\n\n').map((p, n) => (
+      <p key={n}>
+        {p.split('\n').map((l, n) => (
+          <span key={n}>
+            <Linkify properties={{ target: '_blank' }}>{l}</Linkify>
+            <br />
+          </span>
+        ))}
+      </p>
+    ))}
+  </div>
+);
+
+export default ReactionMessage;
