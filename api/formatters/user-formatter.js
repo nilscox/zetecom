19c8fd354ba10e0f28572cf0fd7b5929f69e6585
Nils Layet <nils@epita.fr>
2018-11-22 22:36:52 +0100

    rm old api

--- api/formatters/user-formatter.js
+++
@@ -1,9 +0,0 @@
-const { Formatter } = require('express-extra');
-
-module.exports = Formatter({
-  email: inst => inst.get('email'),
-  nick: inst => inst.get('nick'),
-  avatar: inst => inst.get('avatar'),
-  about: inst => inst.get('about'),
-  signupDate: inst => inst.get('createdAt'),
-});
--- api/labels.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  1: 'SOURCE',
-  2: 'METHOD',
-  3: 'BIAIS',
-  4: 'QUESTION',
-  5: 'OPINION',
-  6: 'FUN',
-};
