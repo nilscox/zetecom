fd398a92de8d9c0ebf35800c5370e1fa8350b5ed
Nils Layet <nils@epita.fr>
2019-03-24 04:51:20 +0100

    feat(frontend): add markdown support for message body

--- frontend/src/components/ReactionContent/ReactionBody.tsx
+++ frontend/src/components/ReactionContent/ReactionBody.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import showdown from 'showdown';
 
 import { Reaction } from '../../types/Reaction';
 
@@ -6,15 +7,18 @@ type ReactionBodyProps = {
   reaction: Reaction;
 };
 
+const converter = new showdown.Converter();
+
 const ReactionBody = (props: ReactionBodyProps) => {
   const { reaction } = props;
+  const html = converter.makeHtml(reaction.text);
 
   return (
     <div>
       { reaction.quote && (
         <div className="reaction-quote">{ reaction.quote }</div>
       ) }
-      <div className="reaction-text">{ reaction.text }</div>
+      <div className="reaction-text markdown-body" dangerouslySetInnerHTML={{ __html: html }} />
     </div>
   );
 };
