49c92a9d2edf328d7928511d332081dbf17519f9
Nils Layet <nils@epita.fr>
2019-03-30 03:47:51 +0100

    feat(frontend): add styles to the history modal

--- frontend/src/components/ReactionContent/ReactionContent.tsx
+++ frontend/src/components/ReactionContent/ReactionContent.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
+import ReactModal from 'react-modal';
 
 import { Reaction, ShortReplyType } from '../../types/Reaction';
 import { ReactionHeader } from './ReactionHeader';
 import { ReactionBody } from './ReactionBody';
 import { ReactionFooter } from './ReactionFooter';
-import { ReactionHistoryModal } from './ReactionHistoryModal';
+import { ReactionHistoryModalContent } from './ReactionHistoryModalContent';
 
 import './ReactionContent.css';
 
@@ -21,8 +22,14 @@ const ReactionContent = (props: ReactionContentProps) => {
 
   return (
     <div id={`reaction-${props.reaction.id}`} className="reaction">
-      <ReactionHeader reaction={props.reaction} onOpenHistory={() => setHistoryModalOpen(true)} />
+
+      <ReactionHeader
+        reaction={props.reaction}
+        onOpenHistory={() => setHistoryModalOpen(true)}
+      />
+
       <ReactionBody reaction={props.reaction} />
+
       <ReactionFooter
         reaction={props.reaction}
         displayReplies={props.displayReplies}
@@ -30,11 +37,17 @@ const ReactionContent = (props: ReactionContentProps) => {
         toggleReplies={props.toggleReplies}
         onShowReplyForm={props.onShowReplyForm}
       />
-      <ReactionHistoryModal
-        isOpen={historyModalOpen}
-        reaction={props.reaction}
-        onRequestClose={() => setHistoryModalOpen(false)}
-      />
+
+      { props.reaction.edited && (
+        <ReactModal
+          isOpen={historyModalOpen}
+          onRequestClose={() => setHistoryModalOpen(false)}
+          closeTimeoutMS={200}
+        >
+          <ReactionHistoryModalContent reaction={props.reaction} />
+        </ReactModal>
+      ) }
+
     </div>
   );
 };
---
+++ frontend/src/components/ReactionContent/ReactionHistoryModalContent.tsx
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+import ReactModal from 'react-modal';
+import moment from 'moment';
+import showdown from 'showdown';
+
+import { classList } from '../../utils/classList';
+import { Reaction, ReactionHistory } from '../../types/Reaction';
+import { fetchReaction } from '../../fetch/fetchReactions';
+import { Loader } from '../Loader';
+
+const converter = new showdown.Converter();
+
+type ReactionHistoryModalContentProps = {
+  reaction: Reaction;
+};
+
+export const ReactionHistoryModalContent = (props: ReactionHistoryModalContentProps) => {
+  const [history, setHistory] = useState<ReactionHistory[]>(undefined);
+  const [fetchingHistory, setFetchingHistory] = useState(false);
+
+  useEffect(() => {
+    if (fetchingHistory || history)
+      return;
+
+    fetchReaction(props.reaction.id)
+      .then((reaction: Reaction) => {
+        setHistory(reaction.history);
+        setFetchingHistory(false);
+      });
+  });
+
+  if (fetchingHistory || !history)
+    return <Loader size="medium" />;
+
+  const getHistory = () => {
+    if (!props.reaction.edited)
+      return [...history].reverse();
+
+    return [
+      { date: props.reaction.edited, text: props.reaction.text },
+      ...[...history].reverse(),
+    ];
+  }
+
+  return (
+    <div>
+      { getHistory().map(({ date, text }, n) => (
+        <div key={date.toString()} className={classList('history-message', n === 0 && 'history-current-message')}>
+          <div className="history-message-date-wrapper">
+            <div className={`history-message-date-filler${n !== 0 ? '-left' : ''}`} />
+            <div className="history-message-date">{ moment(date).format('[Le] Do MMMM YYYY [à] hh:mm') }</div>
+            <div className="history-message-date-filler" />
+          </div>
+          <div className="history-message-text" dangerouslySetInnerHTML={{ __html: converter.makeHtml(text) }} />
+        </div>
+      )) }
+    </div>
+  );
+};
