dae2d2587206b664193d1d8f23f4ae3d4d81b86e
Bopzor <violaine.ballandin@gmail.com>
2019-05-05 02:56:50 +0200

    feat(extension): add loader and refacto error

--- chrome-extension/popup/popup.js
+++ chrome-extension/popup/popup.js
@@ -4,27 +4,44 @@ const inputPassword = document.getElementById('input-password');
 const errorEmail = document.getElementById('error-email');
 const errorPassword = document.getElementById('error-password');
 const errorGlobal = document.getElementById('error-global');
+const loader = document.getElementById('loader');
 const login = document.getElementsByClassName('login')[0];
 const logout = document.getElementsByClassName('logout')[0];
+const loginBtn = document.getElementById('login');
 const logoutBtn = document.getElementById('logout');
 
 class RequestError extends Error {
   constructor(obj) {
     super(RequestError.getMessage(obj));
+    this.error = obj;
     this.name = 'RequestError';
     this.scope = this.setScope(obj);
   }
 
   static getMessage(obj) {
     if (obj.message) {
-      return ERRORS_TRADUCTION[obj.message];
+      return ERRORS_TRADUCTION[obj.message] || ERRORS_TRADUCTION['UNKNOWN'];
     }
 
     if (obj.email && obj.email.isEmail) {
-      return ERRORS_TRADUCTION[obj.email.isEmail];
+      return ERRORS_TRADUCTION[obj.email.isEmail] || ERRORS_TRADUCTION['UNKNOWN'];
     }
 
-    return 'unknown error';
+    return ERRORS_TRADUCTION['UNKNOWN'];
+  }
+
+  getErrorMessageForField(field) {
+    if (this.error[field])
+      return this.message;
+
+    return '&nbsp;';
+  }
+
+  getGlobalErrorMessage() {
+    if (this.error.message)
+      return this.message;
+
+    return '&nbsp;';
   }
 
   setScope(obj) {
@@ -34,6 +51,16 @@ class RequestError extends Error {
     if (obj.email)
       return 'email';
   }
+}
+
+const startLoader = () => {
+  loader.classList.remove('hidden');
+  loginBtn.classList.add('loading');
+}
+
+const stopLoader = () => {
+  loader.classList.add('hidden');
+  loginBtn.classList.remove('loading');
 }
 
 const main = () => {
@@ -52,46 +79,51 @@ form.onsubmit = async (e) => {
   try {
     e.preventDefault();
     await handleSubmit();
-  } catch (e) {
-    console.log(e);
-    console.log(e.scope);
 
-    if (e.scope === 'global') {
-      errorGlobal.innerHTML = e.message;
+  } catch (err) {
+    if (err.name !== 'RequestError') {
+      errorGlobal.innerHTML = ERRORS_TRADUCTION[err.message] || ERRORS_TRADUCTION['UNKNOWN'];
       errorEmail.innerHTML = '&nbsp';
-    }
-
-    if (e.scope === 'email') {
-      errorEmail.innerHTML = e.message;
-      errorGlobal.innerHTML = '&nbsp';
+      errorPassword.innerHTML = '&nbsp';
+    } else {
+      errorEmail.innerHTML = err.getErrorMessageForField('email');
+      errorPassword.innerHTML = err.getErrorMessageForField('password');
+      errorGlobal.innerHTML = err.getGlobalErrorMessage();
     }
   }
 }
 
 const handleSubmit = async () => {
-  const res = await fetch(`https://cdv.localhost/api/auth/login`, {
-    method: 'POST',
-    body: JSON.stringify({
-      email: inputEmail.value,
-      password: inputPassword.value,
-    }),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
+  startLoader();
 
-  if (!res.ok)
-    throw new RequestError(await res.json());
+  try {
+    const res = await fetch(`https://cdv.localhost/api/auth/login`, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: inputEmail.value,
+        password: inputPassword.value,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
 
-  const body = await res.json();
+    if (!res.ok) {
+      throw new RequestError(await res.json());
+    }
 
-  console.log('login success!', body.user);
+    const body = await res.json();
 
-  chrome.storage.local.set({
-    token: body.token,
-  });
+    console.log('login success!', body.user);
 
-  main();
+    chrome.storage.local.set({
+      token: body.token,
+    });
+
+    main();
+  } finally {
+    stopLoader();
+  }
 }
 
 document.addEventListener('DOMContentLoaded', function () {
