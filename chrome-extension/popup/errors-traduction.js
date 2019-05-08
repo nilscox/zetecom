992c646caabbb46f3f5febcd39e3d698b8828e84
Nils Layet <nils@epita.fr>
2019-05-08 19:46:38 +0200

    feat(chrome-extension): use react for the chrome extension

--- chrome-extension/popup/errors-traduction.js
+++
@@ -1,6 +0,0 @@
-const ERRORS_TRADUCTION = {
-  'UNKNOWN': 'Une erreur est survenue.',
-  INVALID_CREDENTIALS: 'Combinaison email / mot de passe invalide.',
-  'email must be an email': 'Format d\'email invalide',
-  'Failed to fetch': 'Erreur de connexion.'
-};
---
+++ chrome-extension/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      Popopopopup !
+    </div>
+  );
+}
+
+export default App;
