0fe01c7237a1f1b8189cc082c5998c6c5e36e5b8
Nils Layet <nils@epita.fr>
2019-06-30 15:11:36 +0200

    feat(code): perform an update in the code

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -109,7 +109,7 @@ const Home: React.FC = () => {
 
       </Section>
 
-      <Note>Work in progress... with ci...</Note>
+      <Note>Work in progress...</Note>
 
       <Section title="Utilisation">
 
