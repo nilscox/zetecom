839453983ff936e80bd8931965edd2ebb8ba622d
Nils Layet <nils@epita.fr>
2019-07-04 00:30:54 +0200

    fix(website): fix lint issue

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -38,15 +38,28 @@ import DownloadExtensionButton from './components/DownloadExtensionButton';
 
 --
 
-  J'approuve : je trouve que cette réaction est pertinente et je suis d'accord avec le message
-    elle apporte des éléments me convaincant
-  Je réfute : je trouve que cette réaction est pertinente mais je ne suis pas d'accord avec le message
-    la méthode utilisée me semble incorrecte ou biaisée
-    j'ai une preuve du contraire
-  Je suis septique : je trouve que cette réaction est pertinente
-    elle n'apporte pas assez d'éléments pour me convaincre
-    j'ai besoin de m'informer plus sur le sujet pour me faire une opinion
-    elle évoque une problématique qui vaut la peine d'être discutée plus en profondeur
+J'approuve : je trouve que cette réaction est pertinente et je suis d'accord avec le message
+  elle apporte des éléments me convaincant
+Je réfute : je trouve que cette réaction est pertinente mais je ne suis pas d'accord avec le message
+  la méthode utilisée me semble incorrecte ou biaisée
+  j'ai une preuve du contraire
+Je suis septique : je trouve que cette réaction est pertinente
+  elle n'apporte pas assez d'éléments pour me convaincre
+  j'ai besoin de m'informer plus sur le sujet pour me faire une opinion
+  elle évoque une problématique qui vaut la peine d'être discutée plus en profondeur
+
+--
+
+Personne n'étant à l'abri d'une erreur, vous pouvez à tout moment modifier le texte d'une réaction. Attention
+cependant à ne pas changer le sens du message ! Autrement, les réactions en réponse à votre message ne seront
+potentiellement plus en harmonie avec le nouveau sens de votre message. Lorsque vous éditez une réaction, une
+étoile apparaît à côté de la date de publication, et il sera toujours possible de consulter l'historique des
+modifications en cliquant sur cette date.
+
+Enfin, après qu'une réaction ait été publiée, il n'est pas possible de l'effacer. Vous avez partagé un message
+à une certaine date, si votre pensée évolue ultérieurement, le message que vous aviez rédigé avant de changer
+d'avis doit rester le même, dans le but de conserver l'évolution du débat et des réponses à votre réaction
+initiale.
 
 --
 
@@ -172,59 +185,6 @@ const Home: React.FC = () => {
 
       <Note>Work in progress...</Note>
 
-{/*
-      <SubTitle>Donner son avis</SubTitle>
-
-      <Section>
-        <p>
-          Donner son avis sur une réaction permet à la plateforme de mieux la référencer lorsque les réactions sont
-          triées par pertinence. Cet avis est totalement subjectif, et il vous est possible d'en changer (ou de le
-          retirer) à tout moment. Plus une réaction a d'avis, et mieux elle est référencée. Les avis que vous pouvez
-          donner sur une réaction sont :
-        </p>
-      </Section>
-
-      <Note>
-        Attention ! Ce n'est pas parce que vous réfutez une réaction qu'elle sera moins bien référencée. Au contraire,
-        une réaction est jugée plus pertinente si elle a un grand nombre d'avis.
-      </Note>
-
-      <SubTitle>Rédiger une réaction</SubTitle>
-
-      <Section>
-        <p>
-          Pour rédiger une réaction, rendez-vous sur une vidéo YouTube ayant les commentaires de CDV activée. Pour le
-          moment, il n'est pas possible d'activer les commentaires de CDV sur une vidéo autrement qu'
-          <a href="mailto:cdv@nils.cx">en passant par un administrateur de la plateforme</a>.
-        </p>
-      </Section>
-
-      <Section>
-        <p>
-          Une réaction se compose de 2 parties :
-        </p>
-
-        <ul>
-          <li>une citation (optionelle), permettant de réagir à une partie précise de la vidéo ;</li>
-          <li>un message, le commentaire à proprement parlé ;</li>
-        </ul>
-
-        <p>
-          Personne n'étant à l'abri d'une erreur, vous pouvez à tout moment modifier le texte d'une réaction. Attention
-          cependant à ne pas changer le sens du message ! Autrement, les réactions en réponse à votre message ne seront
-          potentiellement plus en harmonie avec le nouveau sens de votre message. Lorsque vous éditez une réaction, une
-          étoile apparaît à côté de la date de publication, et il sera toujours possible de consulter l'historique des
-          modifications en cliquant sur cette date.
-        </p>
-
-        <p>
-          Enfin, après qu'une réaction ait été publiée, il n'est pas possible de l'effacer. Vous avez partagé un message
-          à une certaine date, si votre pensée évolue ultérieurement, le message que vous aviez rédigé avant de changer
-          d'avis doit rester le même, dans le but de conserver l'évolution du débat et des réponses à votre réaction
-          initiale.
-        </p>
-      </Section>
-*/}
     </div>
   );
 };
