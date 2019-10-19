58c4e3a3867b73c4a6408cd4b8cca02b152c2c89
Nils <nils@localhost>
2019-10-19 18:56:48 +0200

    feat(website): load client js

--- website/src/components/Link.tsx
+++ website/src/components/Link.tsx
@@ -2,13 +2,7 @@ import React from 'react';
 
 const Link: React.FC<React.HTMLProps<HTMLAnchorElement>> = ({ style, ...props }) => {
   return (
-    <a
-      style={{
-        color: '#333333',
-        ...style,
-      }}
-      {...props}
-    />
+    <a {...props} />
   );
 };
 
