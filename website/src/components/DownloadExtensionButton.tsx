ca08a490776604460a0adc8204ec01e6ee48c2c2
Nils <nils@localhost>
2019-10-19 20:11:14 +0200

    feat(website): fix last css issues

--- website/src/components/DownloadExtensionButton.tsx
+++ website/src/components/DownloadExtensionButton.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { useEnvironment } from 'src';
+
+import { useEnvironment } from 'src/index';
+import Link from './Link';
+
+import './DownloadExtensionButton.scss';
 
 type DownloadExtensionProps = {
   children: string;
@@ -9,34 +13,15 @@ const DownloadExtension: React.FC<DownloadExtensionProps> = ({ children }) => {
   const { CHROME_EXTENSION_URL } = useEnvironment();
 
   return (
-    <div style={{
-      display: 'flex',
-      flexDirection: 'row',
-      justifyContent: 'center',
-      margin: '20px 0',
-      textAlign: 'center',
-    }}>
-      <a
+    <div className="download-extension-container">
+      <Link
         href={CHROME_EXTENSION_URL}
         target="_blank"
         rel="noopener noreferrer"
-        className="download-extension-button"
-        style={{
-          border: '1px solid',
-          borderColor: '#4CAF50',
-          background: '#81c784',
-          padding: '8px 20px',
-          borderRadius: 2,
-          fontSize: '1.2rem',
-          fontWeight: 'bold',
-          textTransform: 'uppercase',
-          textDecoration: 'none',
-          color: '#FFF',
-          transition: 'opacity 200ms',
-        }}
+        className="download-extension"
       >
         { children }
-      </a>
+      </Link>
     </div>
   );
 };
