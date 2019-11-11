937d372645f63e24e0733d4ca0c1156e1face1b9
Nils <nils@epita.fr>
2019-11-11 15:44:19 +0100

    feat(extension): add a header on the integration

--- extension/src/integrations/Integration.tsx
+++ extension/src/integrations/Integration.tsx
@@ -7,6 +7,23 @@ import { useTheme } from 'src/utils/Theme';
 import SubjectsListView from './views/SubjectsListView';
 import SubjectView from './views/SubjectView';
 
+const Header: React.FC = () => {
+  const { fontSizes, colors } = useTheme();
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+
+      <img src="/assets/images/logo.png" alt="Logo de Réagir à l'information" style={{ width: 36, height: 36, opacity: 0.8, marginRight: 10 }} />
+
+      <div>
+        <h1 style={{ fontFamily: 'Domine', fontSize: fontSizes.title }}>Réagir à l'information</h1>
+        <div style={{ color: colors.textLight, fontSize: fontSizes.small, letterSpacing: 4 }}>Décryptons les médias !</div>
+      </div>
+
+    </div>
+  );
+};
+
 type IntegrationProps = {
   information: Information;
 };
@@ -17,6 +34,7 @@ const Integration: React.FC<IntegrationProps> = ({ information }) => {
   return (
     <div style={{ minHeight: 480, backgroundColor: 'white', padding: 10, border: `1px solid ${border}` }}>
       <Router>
+        <Header />
         <Route path="/" exact render={(props) => <SubjectsListView information={information} {...props} />} />
         <Route path="/subject/:id" exact component={SubjectView} />
       </Router>
