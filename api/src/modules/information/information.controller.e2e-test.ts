e98f949f6a7cf9f18fce6be30abe06b7f430b5a2
Nils <nils@epita.fr>
2019-12-08 22:44:07 +0100

    test(api): add tests for the information controller

--- api/src/modules/information/information.controller.e2e-test.ts
+++ api/src/modules/information/information.controller.e2e-test.ts
@@ -1,10 +1,14 @@
 import * as request from 'supertest';
 
 import { InformationModule } from './information.module';
+import { Information } from './information.entity';
+import { Reaction } from '../reaction/reaction.entity';
+import { Message } from '../reaction/message.entity';
 
 import { setupE2eTest } from '../../testing/typeorm/setup-e2e-test';
 import { createInformation } from '../../testing/factories/information.factory';
-import { Information } from './information.entity';
+import { createReaction } from '../../testing/factories/reaction.factory';
+import { createMessage } from '../../testing/factories/message.factory';
 
 describe('information controller', () => {
 
@@ -13,15 +17,39 @@ describe('information controller', () => {
   }, moduleBuilder => {
     moduleBuilder
       .overrideProvider('INFORMATION_PAGE_SIZE')
-      .useValue(1);
+      .useValue(2);
   });
 
   let information1: Information;
   let information2: Information;
+  let information3: Information;
+
+  let message1: Message;
+  let reaction1: Reaction;
+
+  let reaction2: Reaction;
+
+  let message3: Message;
+  let reaction3: Reaction;
+
+  let message4: Message;
+  let reaction4: Reaction;
 
   beforeAll(async () => {
     information1 = await createInformation();
     information2 = await createInformation();
+    information3 = await createInformation();
+
+    message1 = await createMessage({ text: 'message1 search wow' });
+    reaction1 = await createReaction({ information: information1, messages: [message1] });
+
+    reaction2 = await createReaction({ information: information1 });
+
+    message3 = await createMessage({ text: 'searching message3' });
+    reaction3 = await createReaction({ information: information1, messages: [message3], parent: reaction2 });
+
+    message4 = await createMessage({ text: 'message4 search' });
+    reaction4 = await createReaction({ information: information2, messages: [message4] });
   });
 
   describe('list informations', () => {
@@ -32,12 +60,8 @@ describe('information controller', () => {
         .expect(200)
         .then(({ body }) => {
           expect(body).toMatchObject([
-            {
-              id: information1.id,
-              creator: {
-                id: information1.creator.id,
-              },
-            },
+            { id: information1.id },
+            { id: information2.id },
           ]);
         });
     });
@@ -48,12 +72,91 @@ describe('information controller', () => {
         .expect(200)
         .then(({ body }) => {
           expect(body).toMatchObject([
-            {
-              id: information2.id,
-              creator: {
-                id: information2.creator.id,
-              },
-            },
+            { id: information3.id },
+          ]);
+        });
+    });
+
+  });
+
+  describe('get information', () => {
+
+    it('should not find an information with unexisting id', () => {
+      return request(server)
+        .get(`/api/information/4`)
+        .expect(404);
+    });
+
+    it('should fetch an information by id', () => {
+      return request(server)
+        .get(`/api/information/${information1.id}`)
+        .expect(200)
+        .then(({ body }) => {
+          expect(body).toMatchObject({
+            id: information1.id,
+          });
+        });
+    });
+
+    it('should not find an information with unexisting url', () => {
+      return request(server)
+        .get(`/api/information/by-url/${encodeURIComponent('https://fake.news/article/1')}`)
+        .expect(404);
+    });
+
+    it('should fetch an information by url', () => {
+      return request(server)
+        .get(`/api/information/by-url/${encodeURIComponent(information1.url)}`)
+        .expect(200)
+        .then(({ body }) => {
+          expect(body).toMatchObject({
+            id: information1.id,
+          });
+        });
+    });
+
+  });
+
+  describe('get root reactions', () => {
+
+    it('should not find reaction for an information that does not exist', () => {
+      return request(server)
+        .get(`/api/information/4/reactions`)
+        .expect(404);
+    });
+
+    it('should fetch the root reactions', () => {
+      return request(server)
+        .get(`/api/information/${information1.id}/reactions`)
+        .expect(200)
+        .then(({ body }) => {
+          expect(body).toMatchObject([
+            { id: reaction2.id },
+            { id: reaction1.id },
+          ]);
+        });
+    });
+
+    it('should fetch the root reactions sorted by date asc', () => {
+      return request(server)
+        .get(`/api/information/${information1.id}/reactions?sort=date-asc`)
+        .expect(200)
+        .then(({ body }) => {
+          expect(body).toMatchObject([
+            { id: reaction1.id },
+            { id: reaction2.id },
+          ]);
+        });
+    });
+
+    it('should search from the reactions', () => {
+      return request(server)
+        .get(`/api/information/${information1.id}/reactions?search=search`)
+        .expect(200)
+        .then(({ body }) => {
+          expect(body).toMatchObject([
+            { id: reaction3.id },
+            { id: reaction1.id },
           ]);
         });
     });
--- api/src/testing/factories/reaction.factory.ts
+++ api/src/testing/factories/reaction.factory.ts
@@ -2,10 +2,14 @@ import { DeepPartial, getManager } from 'typeorm';
 
 import { Reaction } from '../../modules/reaction/reaction.entity';
 
+import { createUser } from './user.factory';
 import { createMessage } from './message.factory';
 
 export const createReaction = async (data: DeepPartial<Reaction> = {}) => {
   const manager = await getManager();
+
+  if (!data.author)
+    data.author = await createUser();
 
   if (!data.messages)
     data.messages = [await createMessage()];
