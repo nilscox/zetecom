974d6a4bbd568eeebbe7766a93fde48b4e28382e
Bopzor <violaine.ballandin@gmail.com>
2020-01-19 00:04:42 +0100

    feat(notification): create notifications count endpoints

---
+++ api/src/modules/notification/notification.controller.e2e-test.ts
@@ -0,0 +1,57 @@
+import * as request from 'supertest';
+import { Repository, getRepository, In } from 'typeorm';
+
+import { setupE2eTest, createAuthenticatedUser } from '../../testing/setup-e2e-test';
+import { AuthenticationModule } from '../authentication/authentication.module';
+import { NotificationModule } from './notification.module';
+import { Notification } from './notification.entity';
+import { createSubscription } from '../../testing/factories/subscription.factory';
+import { createNotification } from '../../testing/factories/notification.factory';
+
+describe('notifications', () => {
+
+  const server = setupE2eTest({
+    imports: [AuthenticationModule, NotificationModule],
+  });
+
+  const { user, authRequest } = createAuthenticatedUser(server);
+
+  let notificationRepository: Repository<Notification>;
+
+  beforeAll(() => {
+    notificationRepository = getRepository(Notification);
+  });
+
+  describe('get count', () => {
+
+    it('should not fetch notifications count when not authenticated', () => {
+      return request(server)
+        .get('/api/notification/me/count')
+        .expect(403);
+    });
+
+    it('should fetch unseen notifications count', async () => {
+      const subscription = await createSubscription({ user });
+      await createNotification({ subscription });
+
+      const { body } = await authRequest
+        .get('/api/notification/me/count')
+        .expect(200);
+
+      expect(body).toMatchObject({ count: 1 });
+    });
+
+    it('should fetch seen notifications count', async () => {
+      const subscription = await createSubscription({ user });
+      await createNotification({ subscription, seen: new Date() });
+
+      const { body } = await authRequest
+        .get('/api/notification/me/seen/count')
+        .expect(200);
+
+      expect(body).toMatchObject({ count: 1 });
+    });
+
+  });
+
+});
