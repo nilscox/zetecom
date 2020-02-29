824559d3173c367a18993b4b52ee720a064b0f7d
Nils <nils@epita.fr>
2020-02-29 20:57:08 +0100

    feat(extension): setup storybook

---
+++ extension/.storybook/main.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+const babelConfig = require('../babel.config');
+
+module.exports = {
+
+  stories: ['../src/**/*.stories.tsx'],
+
+  webpackFinal: (config) => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      use: [
+        {
+          loader: require.resolve('babel-loader'),
+          options: babelConfig,
+        },
+        // Optional
+        {
+          loader: require.resolve('react-docgen-typescript-loader'),
+        },
+      ],
+    });
+
+    config.resolve.extensions.push('.ts', '.tsx');
+    config.resolve.alias['react-dom'] = '@hot-loader/react-dom';
+    config.resolve.alias.src = path.resolve(__dirname, '..', 'src');
+
+    console.dir(config, { depth: null });
+
+    return config;
+  },
+
+};
