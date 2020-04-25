a6f1b99cf7a9b84f7094979b8649646f27e4ce78
Nils <nils@epita.fr>
2020-04-25 16:06:21 +0200

    style(home): remove max line length constraint

--- website/src/pages/Home.tsx
+++ website/src/pages/Home.tsx
@@ -19,44 +19,11 @@ import imageMarkdown from 'src/images/markdown.png';
 
 import './Home.scss';
 
-/*
-
-Réagir à l'information
-Décryptons l'information !
-
-En 10 mots:
-  - Réagir à l'information est un espace de discussions collaboratives en réaction à l'information
-  - Réagir à l'information est un espace d'échange collaboratif en réaction à l'information
-
-Pourquoi ?
-  - Rassembler une communauté de personnes pour réfléchir ensemble à ce que disent les médias
-  - Lutter contre les fausses informations
-  - Offrir une place aux débats dans un climat de confiance sur la toile
-
-Comment ?
-  - Des zones de commentaires liées à Réagir à l'information intégrées sur les sites d'information
-  - Un tri des commentaires par pertinence
-  - Un cadre saint et rigoureux dans les échanges, instauré par une charte
-  - Des messages correctement formatés pour maximiser la clarté des propos
-  - Une modération des débats faite par des volontaires de la communauté
-
-Qu'est-ce que c'est ?
-  - Une communauté
-  - Une extension pour les navigateurs
-  - Un contrat d'utilisation (la charte)
-  - Un algorithme de référencement
-  - Une hiérarchie des réactions
-  - Un support markdown
-  - Une charte pour la modération
-
-*/
-
 const Home: React.FC = () => (
   <>
 
     <div className="heading">
-      <Link href="/"><em>Réagir à l'information</em></Link>, c'est un <strong>espace d'échange collaboratif</strong>,
-      pour discuter de l'information diffusée par les médias sur internet.
+      <Link href="/"><em>Réagir à l'information</em></Link>, c'est un <strong>espace d'échange collaboratif</strong>, pour discuter de l'information diffusée par les médias sur internet.
     </div>
 
     <Title id="Les objectifs">Les objectifs ?</Title>
@@ -82,22 +49,16 @@ const Home: React.FC = () => (
     <div className="why">
 
       <p>
-        Depuis quelques dizaines d'années, les évolutions technologiques ont enclenché une vraie{' '}
-        <Link openInNewTab href="https://fr.wikipedia.org/wiki/R%C3%A9volution_num%C3%A9rique">révolution</Link>, qui a
-        radicalement bouleversé notre façon de communiquer <em>et de nous informer</em>.<br />
-        En contrepartie, un nombre
-        croissant de problématiques liées à l'information émergent, notamment dans la diffusion de celle-ci par les
-        médias. Les articles relatant des faits hors du commun étant plus attrayants, les contenus sont parfois plus
-        sensationnalistes que vrais.
+        Depuis quelques dizaines d'années, les évolutions technologiques ont enclenché une vraie <Link openInNewTab href="https://fr.wikipedia.org/wiki/R%C3%A9volution_num%C3%A9rique">révolution</Link>, qui a radicalement bouleversé notre façon de communiquer <em>et de nous informer</em>.<br />
+        En contrepartie, un nombre croissant de problématiques liées à l'information émergent, notamment dans la diffusion de celle-ci par les médias.
+        Les articles relatant des faits hors du commun étant plus attrayants, les contenus sont parfois plus sensationnalistes que vrais.
       </p>
 
       <div className="separator" />
 
       <p>
-        Mais avons-nous <strong>les bons outils</strong> pour réfléchir ensemble, intelligemment, face à cette abondance
-        d'information ?<br />
-        Réagir à l'information a pour ambition d'apporter des solutions à ces problèmes, en proposant une plateforme qui
-        <em>vous</em> permet de réagir aux médias sur internet, comme des articles de presse ou des vidéos sur YouTube.
+        Mais avons-nous <strong>les bons outils</strong> pour réfléchir ensemble, intelligemment, face à cette abondance d'information ?<br />
+        Réagir à l'information a pour ambition d'apporter des solutions à ces problèmes, en proposant une plateforme qui <em>vous</em> permet de réagir aux médias sur internet, comme des articles de presse ou des vidéos sur YouTube.
       </p>
 
     </div>
@@ -108,12 +69,10 @@ const Home: React.FC = () => (
 
       <div className="integration-text">
         <h3>
-          Une exension chrome va <strong>intégrer</strong> des zones de commentaires sur les sites d'information que
-          vous visitez.
+          Une exension chrome va <strong>intégrer</strong> des zones de commentaires sur les sites d'information que vous visitez.
         </h3>
         <h3>
-          Ces commentaires doivent respecter <Link href="/charte.html">une charte</Link>, construite dans le but de
-          favoriser des échanges argumentatifs et respectueux.
+          Ces commentaires doivent respecter <Link href="/charte.html">une charte</Link>, construite dans le but de favoriser des échanges argumentatifs et respectueux.
         </h3>
       </div>
 
@@ -128,7 +87,7 @@ const Home: React.FC = () => (
         <div className="what-image">
           <img src={imageSearch} />
         </div>
-        <h4 className="what-text">Recherche parmis les réactions</h4>
+        <h4 className="what-text">Recherche</h4>
       </div>
       <div className="what-item">
         <div className="what-image">
@@ -154,20 +113,18 @@ const Home: React.FC = () => (
         </div>
         <h4 className="what-text">Messages structurés</h4>
       </div>
-      {/* <div className="what-item">
-        <div className="what-image"></div>
-        <h4 className="what-text">Modération assurée par des personnes volontaires</h4>
-      </div> */}
     </div>
 
     <DownloadExtension>Installer l'extension chrome</DownloadExtension>
 
     <p>
-      Nous tentons de rassembler sur cette plateforme, des personnes bienveillantes, attentives aux biais, qui savent
-      écouter et partager leurs opinions, tout en gardant un œil critique face aux arguments qu'on leur présente.
-      Si vous voulez mieux comprendre l'information et participer à des échanges constructifs sur internet, prenez une
-      seconde pour ajouter un marque page, et pourquoi pas{' '}
-      <Link href="/utilisation.html#Inscription">vous inscrire</Link> !
+      L'extension peut être utilisée pour lire les messages présents sur la plateforme sans avoir besoin de créer de compte.
+      Pour en savoir plus, la page <Link href="/utilisation.html">utilisation</Link> présente quelques captures d'écran et explique comment fonctionne cet outil.
+      Si vous souhaitez mieux comprendre le contexte dans lequel se place Réagir à l'information, et les objectifs du projet, rendez-vous sur la page <Link href="/motivations.html">motivations</Link>.
+    </p>
+
+    <p>
+      Nous tentons de rassembler sur cette plateforme, des personnes bienveillantes, attentives aux biais, qui savent écouter et partager leurs opinions, tout en gardant un œil critique face aux arguments qu'on leur présente. Prêt(e) à tenter l'expérience ?
     </p>
 
   </>
