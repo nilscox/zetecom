f0fa8d6e2028f94713011a97df95fe3a8b3d4450
Nils <nils@epita.fr>
2020-04-26 14:48:22 +0200

    feat(carousel): responsive

--- website/src/components/Carousel.tsx
+++ website/src/components/Carousel.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 
 import { animated, useSpring, config } from 'react-spring';
+import { useSwipeable } from 'react-swipeable';
 
 import './Carousel.scss';
 
@@ -31,28 +32,43 @@ const Carousel: React.FC<CarouselProps> = ({ width, delay, slides }) => {
     setAutoplay(false);
   };
 
+  const prevSlide = () => setSlide(s => (s - 1 + slides.length) % slides.length);
+  const nextSlide = () => setSlide(s => (s + 1) % slides.length);
+
+  const swipeHandler = useSwipeable({
+    onSwipedRight: prevSlide,
+    onSwipedLeft: nextSlide,
+  });
+
   return (
-    <div className="carousel" style={{ width }}>
-      <svg viewBox="0 0 32 32" className="prev-slide" onClick={() => setSlide(s => (s - 1 + slides.length) % slides.length)}>
+    <div className="carousel" style={{ width }} {...swipeHandler}>
+
+      <svg viewBox="0 0 32 32" className="prev-slide" onClick={prevSlide}>
         <path d="M14.19 16.005l7.869 7.868-2.129 2.129-9.996-9.997L19.937 6.002l2.127 2.129z" fill="currentColor" />
       </svg>
+
       <div className="slides-container">
         <animated.div className="slider" style={spring}>
           { slides.map(({ text, image }, n) => (
             <div key={n} className="slide" style={{ width }}>
+
               <div className="slide-text">
                 <strong>{ text }</strong>
               </div>
+
               <div className="slide-image">
                 <img src={image} />
               </div>
+
             </div>
           )) }
         </animated.div>
       </div>
-      <svg viewBox="0 0 32 32" className="next-slide"  onClick={() => setSlide(s => (s + 1) % slides.length)}>
+
+      <svg viewBox="0 0 32 32" className="next-slide"  onClick={nextSlide}>
         <path d="M18.629 15.997l-7.083-7.081L13.462 7l8.997 8.997L13.457 25l-1.916-1.916z" fill="currentColor" />
       </svg>
+
       <div className="carousel-dots">
         { slides.map((_, n) => (
           <svg key={n} className="carousel-dot" width={16} height={16} version="1.1">
@@ -60,6 +76,7 @@ const Carousel: React.FC<CarouselProps> = ({ width, delay, slides }) => {
           </svg>
         )) }
       </div>
+
     </div>
   );
 };
