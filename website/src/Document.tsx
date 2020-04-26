58e57139f8af3e5d941cd6c16012885e7a81f2e7
Nils <nils@epita.fr>
2020-04-26 11:01:31 +0200

    feat(website): hydrate app

---
+++ website/src/Document.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+import { Page } from './pages';
+import App from './App';
+import { useEnvironment } from './index';
+
+type DocumentProps = {
+  locals: any;
+  page: Page;
+};
+
+const Document: React.FC<DocumentProps> = (props) => (
+  <html lang="fr">
+    <head>
+
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="description" content="Réagir à l'information sur internet" />
+
+      <title>Réagir à l'information</title>
+
+      <link type="text/css" rel="stylesheet" href="/assets/css/reset.css" />
+      <link type="text/css" rel="stylesheet" href="/assets/css/styles.css" />
+
+    </head>
+    <body>
+
+      <div id="app">
+        <App {...props} />
+      </div>
+
+      <script type="text/javascript" src="/assets/js/client.js" />
+
+      { useEnvironment('NODE_ENV') === 'development' && (
+        <script src={useEnvironment('WEBSITE_URL') + '/webpack-dev-server.js'} />
+      ) }
+
+      { useEnvironment('NODE_ENV') === 'production' && (
+        <>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-143537578-1" />
+          <script type="text/javascript" src="/assets/js/gtag.js" />
+        </>
+      ) }
+
+      <script type="text/javascript" src="/index.js" />
+
+    </body>
+  </html>
+);
+
+export default Document;
