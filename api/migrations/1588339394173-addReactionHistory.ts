1a527b29df8ca4a86a571d68bf81215ec240ed95
Nils <nils@epita.fr>
2020-05-01 19:43:53 +0200

    feat(history): set message_id to existing reactions

--- api/migrations/1588339394173-addReactionHistory.ts
+++ api/migrations/1588339394173-addReactionHistory.ts
@@ -11,6 +11,13 @@ export class addReactionHistory1588339394173 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "reaction" ADD "message_id" integer`, undefined);
         await queryRunner.query(`ALTER TABLE "reaction" ADD CONSTRAINT "UQ_9276e74bb0d8be2c75ec3b51e61" UNIQUE ("message_id")`, undefined);
         await queryRunner.query(`ALTER TABLE "reaction" ADD CONSTRAINT "FK_9276e74bb0d8be2c75ec3b51e61" FOREIGN KEY ("message_id") REFERENCES "message"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`, undefined);
+
+        const reactions = await queryRunner.query('SELECT reaction.id from reaction');
+
+        await Promise.all(reactions.map(async (reaction) => {
+            const [lastMessage] = await queryRunner.query('SELECT message.id from message WHERE message.reaction_id = ' + reaction.id + ' ORDER BY message.created DESC LIMIT 1');
+            await queryRunner.query('UPDATE reaction SET message_id = ' + lastMessage.id + ' WHERE reaction.id = ' + reaction.id);
+        }));
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
