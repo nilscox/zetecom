77e65df311b0215b3c14f510516378d60bfd8156
Nils <nils@epita.fr>
2020-05-02 02:10:24 +0200

    chore: cleanup environment variables

--- extension/src/utils/env.ts
+++ extension/src/utils/env.ts
@@ -2,8 +2,7 @@ const env = {
   NODE_ENV: process.env.NODE_ENV,
   API_URL: process.env.API_URL,
   EXTENSION_URL: process.env.EXTENSION_URL,
-  CHROME_EXTENSION_URL: process.env.CHROME_EXTENSION_URL,
-  REPOSITORY_URL: process.env.REPOSITORY_URL,
+  WEBSITE_URL: process.env.WEBSITE_URL,
 };
 
 export type EnvironmentVariable = keyof typeof env;
