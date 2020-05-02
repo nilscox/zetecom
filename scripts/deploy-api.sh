8a0062f912631233467af052f240df35c28c204a
Nils <nils@epita.fr>
2020-05-02 03:27:42 +0200

    feat(deployment): add deployment scripts

---
+++ scripts/deploy-api.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+source $(dirname "$0")/functions.sh
+environment=$1
+
+prepare_deployment() {
+  if [ "$environment" != 'production' ] && [ "$environment" != 'staging' ]; then
+    echo 'usage: deploy-api.sh [staging|production]' >&2
+    exit 1
+  fi
+
+  echo "Environment: $environment"
+}
+
+setup_environment () {
+  load_env deploy.env
+  load_env api.env
+  echo
+
+  echo "Environment variables:"
+  echo_env api.env
+  echo
+
+  echo "Deployment variables:"
+  echo_vars deploy_user deploy_host base_dir api_image api_port
+}
+
+deploy_api() {
+  execute ssh "$deploy_user@$deploy_host" docker pull "$api_image"
+  execute ssh "$deploy_user@$deploy_host" docker rm -f "ri-api-$environment" || true
+  execute ssh "$deploy_user@$deploy_host" docker run -dt \
+    --name "ri-api-$environment" \
+    --network "ri-network-$environment" \
+    --volume "$base_dir/avatars:/app/avatars:rw" \
+    --volume "$base_dir/email-templates:/app/email-templates:ro" \
+    -p "$api_port:80" \
+    --env $(sshenv REFLECT_ORIGIN) \
+    --env $(sshenv BYPASS_AUTHORIZATIONS) \
+    --env $(sshenv EXTENSION_URL) \
+    --env $(sshenv WEBSITE_URL) \
+    --env $(sshenv DB_HOST) \
+    --env $(sshenv DB_PORT) \
+    --env $(sshenv DB_USER) \
+    --env $(sshenv DB_PASS) \
+    --env $(sshenv DB_NAME) \
+    --env $(sshenv DB_SYNC) \
+    --env $(sshenv DB_ENTITIES) \
+    --env $(sshenv DB_MIGRATIONS_DIR) \
+    --env $(sshenv DB_DEBUG) \
+    --env $(sshenv EMAIL_HOST) \
+    --env $(sshenv EMAIL_USER) \
+    --env $(sshenv EMAIL_PASSWORD) \
+    --env $(sshenv EMAIL_BYPASS) \
+    --env $(sshenv EMAIL_ACCOUNT_VERIFICATION) \
+    --env $(sshenv EMAIL_ACCOUNT_AUTHORIZATION) \
+    --env $(sshenv SESSION_SECRET) \
+    --env $(sshenv EMAIL_TEMPLATE_DIR) \
+    --env $(sshenv USER_AVATAR_DESTINATION) \
+    "$api_image"
+}
+
+main() {
+  echo_title "Deploy API"
+  step "Prepare deployment" prepare_deployment
+  step "Setup environment" setup_environment
+  step "Deploy API" deploy_api
+  echo "Deployment success"
+}
+
+main
---
+++ scripts/deploy-website.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+source $(dirname "$0")/functions.sh
+environment=$1
+
+prepare_deployment() {
+  if [ "$environment" != 'production' ] && [ "$environment" != 'staging' ]; then
+    err 'usage: deploy-website.sh [staging|production]'
+  fi
+
+  if ! grep '"name": "reagir-information-website"' package.json > /dev/null 2>&1;  then
+    err 'wrong directory'
+  fi
+
+  echo "Environment: $environment"
+}
+
+setup_environment () {
+  load_env deploy.env
+  set -a
+  load_env website.env
+  set +a
+  echo
+
+  echo "Environment variables:"
+  echo_env website.env
+  echo
+
+  echo "Deployment variables:"
+  echo_vars deploy_user deploy_host website_dir
+}
+
+build_website() {
+  execute yarn clean
+  execute NODE_ENV='production' yarn build --silent
+}
+
+deploy_website() {
+  execute ssh-add ~/.ssh/ri-deploy
+
+  # assert that the target directory is correct to avoid undesired upcoming `rm -rf`
+  if ! ssh "$deploy_user@$deploy_host" ls "$website_dir" | grep charte.html > /dev/null; then
+    execute ssh-add -d ~/.ssh/ri-deploy
+    err "charte.html not found in $deploy_host:$website_dir aborting deployment"
+  fi
+
+  execute ssh "$deploy_user@$deploy_host" rm -rf "$website_dir"
+  execute scp -r dist "$deploy_user@$deploy_host:$website_dir"
+
+  execute ssh-add -d ~/.ssh/ri-deploy
+}
+
+main() {
+  echo_title "Deploy website"
+  step "Prepare deployment" prepare_deployment
+  step "Setup environment" setup_environment
+  step "Build website" build_website
+  step "Deploy website" deploy_website
+  echo "Deployment success"
+}
+
+main
