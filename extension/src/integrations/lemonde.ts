056292a8f22413ac41562a5e66d62a5da28e37f8
Nils <nils@epita.fr>
2020-05-13 23:49:14 +0200

    chore: rename chrome-extension to extension

---
+++ extension/src/integrations/lemonde.ts
@@ -0,0 +1,8 @@
+// import setupIntegration from '../integration';
+
+// window.addEventListener('DOMContentLoaded', () => {
+//   setupIntegration({
+//     getElementToAppendAfter: () => document.getElementsByClassName('article__content')[0],
+//     pageUrl: window.location.href,
+//   });
+// });
---
+++ extension/src/integrations/skeptikon.ts
@@ -0,0 +1,24 @@
+// import setupIntegration from '../integration';
+
+// setupIntegration({
+//   getElementToSwitchWith: () => document.querySelector('my-video-comments'),
+//   pageUrl: window.location.href,
+//   otherText: 'Commentaires Skepitkón',
+//   riText: 'Commentaires Réagir à l\'information',
+//   buttonsStyles: {
+//     common: {
+//       'font-size': '18px',
+//       'color': '#ffffff',
+//     },
+//     selected: {
+//       'background-color': '#f1680d',
+//       'cursor': 'initial',
+//       'font-weight': 'bold',
+//     },
+//     unselected: {
+//       'background-color': '#404040',
+//       'cursor': 'pointer',
+//       'font-weight': 'initial',
+//     },
+//   },
+// });
