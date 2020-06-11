84ee66f2bb5ce18297a89d256c7aa9b7373f8a54
Nils <nils@epita.fr>
2020-06-12 00:59:53 +0200

    fix(extension): replace name in build script

--- scripts/build-extension.ci.sh
+++ scripts/build-extension.ci.sh
@@ -7,7 +7,7 @@ replace_name_in_manifest() {
   manifest="$1"
 
   if [ "$environment" == 'staging' ]; then
-    sed -i $'s/"name": "Réagir à l\'information"/"name": "Réagir à l\'information (staging)"/' $manifest
+    sed -i $'s/"name": "Zétécom"/"name": "Zétécom (staging)"/' $manifest
   fi
 }
 
@@ -15,8 +15,8 @@ sources_zip_filename="zetecom-extension-$environment-sources-$(package_version .
 zip_filename="zetecom-extension-$environment-$(package_version ./package.json).zip"
 
 create_source_archive() {
-  echo APP_URL="$APP_URL" > .env
   echo NODE_ENV=production > .env
+  echo APP_URL="$APP_URL" > .env
   replace_name_in_manifest manifest.json
   zip_directory "./$sources_zip_filename" .
 }
@@ -26,8 +26,7 @@ build_extension_from_sources() {
 
   unzip "$sources_zip_filename" -d "$tmp_dir"
 
-  (cd "$tmp_dir" && yarn && yarn build --silent)
-  replace_name_in_manifest "$tmp_dir/dist/manifest.json"
+  (cd "$tmp_dir" && yarn && yarn build)
   zip_directory "./$zip_filename" "$tmp_dir/dist"
 
   rm -rf "$tmp_dir"
