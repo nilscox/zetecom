eeb5d8aa37e440562456c7c8bd7b7f60b92250fb
Nils <nils@epita.fr>
2020-06-13 18:07:39 +0200

    feat(app): replace name in the app

--- app/src/components/AuthenticationForm/AuthenticationMessage.tsx
+++ app/src/components/AuthenticationForm/AuthenticationMessage.tsx
@@ -8,9 +8,9 @@ import { Box, Typography } from '@material-ui/core';
 
 /* eslint-disable max-len */
 const text: { [key in Form]: React.ReactNode } = {
-  login: <>Connectez-vous sur <WebsiteLink to="/">Réagir à l'information</WebsiteLink> pour interagir avec le reste de la communauté.</>,
-  signup: <>Créez votre compte sur Réagir à l'information. Vos <WebsiteLink to="/faq.html#donnees-personnelles">données personnelles</WebsiteLink> ne seront pas communiquées en dehors de la plateforme.</>,
-  emailLogin: <>Identifiez-vous sur Réagir à l'information via un email contenant un lien de connexion sans mot de passe.</>,
+  login: <>Connectez-vous sur <WebsiteLink to="/">Zétécom</WebsiteLink> pour interagir avec le reste de la communauté.</>,
+  signup: <>Créez votre compte sur Zétécom. Vos <WebsiteLink to="/faq.html#donnees-personnelles">données personnelles</WebsiteLink> ne seront pas communiquées en dehors de la plateforme.</>,
+  emailLogin: <>Identifiez-vous sur Zétécom via un email contenant un lien de connexion sans mot de passe.</>,
 };
 /* eslint-enable max-len */
 
