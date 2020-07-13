2585003a07ff01cfd743255ca095a33970ebd1fd
Nils <nils@epita.fr>
2020-07-13 20:14:40 +0200

    fix(app): fix sed in build output

--- app/start.sh
+++ app/start.sh
@@ -8,8 +8,8 @@ fi
 
 sed -i "s|__ROOT_DIR__|$root|" /etc/nginx/conf.d/default.conf
 
-sed -i "s|__API_URL__|$API_URL|g" "$root/main.*.js"
-sed -i "s|__WEBSITE_URL__|$WEBSITE_URL|g" "$root/main.*.js"
-sed -i "s|__GOOGLE_ANALYTICS_ID__|$GOOGLE_ANALYTICS_ID|g" "$root/main.*.js"
+sed -i "s|__API_URL__|$API_URL|g" "$root"/main.*.js
+sed -i "s|__WEBSITE_URL__|$WEBSITE_URL|g" "$root"/main.*.js
+sed -i "s|__GOOGLE_ANALYTICS_ID__|$GOOGLE_ANALYTICS_ID|g" "$root"/main.*.js
 
 exec nginx -g 'daemon off;'
