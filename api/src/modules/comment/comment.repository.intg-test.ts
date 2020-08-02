ed4ac90d267d9bfe00913017e758eaf7ffbc1a7a
Nils <nils@epita.fr>
2020-08-02 16:53:27 +0200

    test(api): fix intg tests

--- api/src/modules/comment/comment.repository.intg-test.ts
+++ api/src/modules/comment/comment.repository.intg-test.ts
@@ -1,48 +1,16 @@
-import { DeepPartial, getCustomRepository, getManager } from 'typeorm';
+import { getCustomRepository } from 'typeorm';
 
 import { SortType } from 'Common/sort-type';
 
-import { createInformation } from '../../testing/factories/information.factory';
-import { createMessage } from '../../testing/factories/message.factory';
-import { createReaction } from '../../testing/factories/reaction.factory';
-import { createUser } from '../../testing/factories/user.factory';
+import { createComment } from '../../testing/intg-factories/comment.factory';
+import { createInformation } from '../../testing/intg-factories/information.factory';
+import { createMessage } from '../../testing/intg-factories/message.factory';
+import { createReaction } from '../../testing/intg-factories/reaction.factory';
+import { createUser } from '../../testing/intg-factories/user.factory';
 import { setupIntgTest } from '../../testing/setup-intg-test';
 
-import { Comment } from './comment.entity';
 import { CommentRepository } from './comment.repository';
 import { ReactionType } from './reaction.entity';
-
-export const createComment = async (data: DeepPartial<Comment> = {}) => {
-  const manager = getManager();
-
-  if (!data.information)
-    data.information = await createInformation();
-
-  if (!data.author)
-    data.author = await createUser();
-
-  if (!data.message)
-    data.message = await createMessage();
-
-  const comment = manager.create(Comment, {
-    ...data,
-  });
-
-  const result = await manager.save(comment);
-
-  if (data.history) {
-    await Promise.all(data.history.map(message => {
-      message.comment = result;
-      return manager.save(message);
-    }));
-  }
-
-  data.message.comment = comment;
-  await manager.save(data.message);
-  await manager.query(`UPDATE message SET created = NOW() WHERE id = ${data.message.id}`);
-
-  return result;
-};
 
 describe('comment repository', () => {
 
