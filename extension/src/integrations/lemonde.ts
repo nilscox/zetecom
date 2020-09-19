bcb74a62559704e768934a6c314453da749cc33e
Nils <nils@epita.fr>
2020-09-19 23:38:30 +0200

    fix(extension): fix lemonde regexp

--- extension/src/integrations/lemonde.ts
+++ extension/src/integrations/lemonde.ts
@@ -2,7 +2,7 @@ import setupIntegration from '../integration';
 
 const getElement = () => document.getElementsByClassName('article__content')[0] as HTMLElement;
 
-const LEMONDE_REGEXP = /lemonde.fr\/([a-z]+)\/[a-z]+\/(\d{4}\/\d{2}\/\d{2})\/[-a-z]+_([0-9_]+).html/;
+const LEMONDE_REGEXP = /lemonde\.fr\/([a-z]+)\/[a-z]+\/(\d{4}\/\d{2}\/\d{2})\/[-a-z0-9]+_([0-9_]+)\.html/;
 
 const getIdentifier = () => {
   const match = LEMONDE_REGEXP.exec(window.location.href);
