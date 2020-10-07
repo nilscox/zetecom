f310ce10f7e9620f40bcc4fa692f1301e0c829e9
Nils <nils@epita.fr>
2020-10-07 22:01:35 +0200

    chore(deps): upgrade storybook to version 6

--- app/.storybook/main.js
+++ app/.storybook/main.js
@@ -1,7 +1,5 @@
 const path = require('path');
 
-const babelConfig = require('../babel.config');
-
 module.exports = {
 
   addons: [
@@ -12,24 +10,7 @@ module.exports = {
   stories: ['../src/**/*.stories.tsx'],
 
   webpackFinal: (config) => {
-    config.module.rules.push({
-      test: /\.(ts|tsx)$/,
-      use: [
-        {
-          loader: require.resolve('babel-loader'),
-          options: babelConfig,
-        },
-        // Optional
-        {
-          loader: require.resolve('react-docgen-typescript-loader'),
-        },
-      ],
-    });
-
-    config.resolve.extensions.push('.ts', '.tsx');
-    config.resolve.alias['react-dom'] = '@hot-loader/react-dom';
     config.resolve.alias.src = path.resolve(__dirname, '..', 'src');
-
     return config;
   },
 
