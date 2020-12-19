8770cdcc3d9b84f029035d496af0b611dd509002
Nils <nils@epita.fr>
2020-12-19 17:46:01 +0100

    feat(app): change markdown message edition tabs styles

--- app/src/components/MarkdownMessageEdition/Tabs.tsx
+++ app/src/components/MarkdownMessageEdition/Tabs.tsx
@@ -2,41 +2,32 @@ import React from 'react';
 
 import { Button, Grid, makeStyles, Theme } from '@material-ui/core';
 
-const useStyles = makeStyles<Theme, { active?: boolean }>(
-  ({ breakpoints, spacing, palette: { border, ...palette } }) => ({
-    tab: ({ active }: { active: boolean }) => ({
-      borderTop: `1px solid ${border.main}`,
-      borderRight: `1px solid ${border.main}`,
-      borderLeft: `1px solid ${border.main}`,
-      ...(!active && {
-        borderBottom: `1px solid ${border.main}`,
-      }),
+const useStyles = makeStyles<Theme, { active?: boolean }>(({ breakpoints, spacing, palette }) => ({
+  container: {
+    borderBottom: `1px solid ${palette.border.main}`,
+  },
+  tab: ({ active }: { active: boolean }) => ({
+    margin: spacing(0, 2),
+    transition: 'border-bottom-color 120ms ease-in-out',
+    borderBottom: '2px solid',
+    borderBottomColor: 'transparent',
+    ...(active && {
+      borderBottomColor: palette.primary.main,
     }),
-    separator: {
-      width: spacing(4),
-      borderBottom: `1px solid ${border.main}`,
-      [breakpoints.down('xs')]: {
-        width: spacing(2),
-      },
-    },
-    filler: {
-      flex: 1,
-      borderBottom: `1px solid ${border.main}`,
-    },
-    tabButton: {
-      padding: spacing(2, 4),
-      [breakpoints.down('xs')]: {
-        padding: spacing(1, 3),
-        lineHeight: 0.8,
-      },
-      '&:hover': {
-        background: 'none',
-        color: palette.secondary.main,
-        transition: 'color 200ms ease-in-out',
-      },
-    },
   }),
-);
+  tabButton: {
+    padding: spacing(1, 4),
+    [breakpoints.down('xs')]: {
+      padding: spacing(1, 3),
+      lineHeight: 0.8,
+    },
+    '&:hover': {
+      background: 'none',
+      color: palette.secondary.main,
+      transition: 'color 200ms ease-in-out',
+    },
+  },
+}));
 
 type TabProps = {
   active: boolean;
@@ -49,18 +40,6 @@ const Tab: React.FC<TabProps> = ({ active, children }) => {
   return <div className={classes.tab}>{children}</div>;
 };
 
-const TabSeparator = () => {
-  const classes = useStyles({});
-
-  return <div className={classes.separator} />;
-};
-
-const TabFiller = () => {
-  const classes = useStyles({});
-
-  return <div className={classes.filler} />;
-};
-
 type TabsProps = {
   currentTab: 'edit' | 'preview';
   setCurrentTab: (tab: 'edit' | 'preview') => void;
@@ -70,24 +49,17 @@ const Tabs: React.FC<TabsProps> = ({ currentTab, setCurrentTab }) => {
   const classes = useStyles({});
 
   return (
-    <Grid container>
-      <TabSeparator />
-
+    <Grid container className={classes.container}>
       <Tab active={currentTab === 'edit'}>
         <Button disableRipple className={classes.tabButton} onClick={() => setCurrentTab('edit')}>
           Editer
         </Button>
       </Tab>
-
-      <TabSeparator />
-
       <Tab active={currentTab === 'preview'}>
         <Button disableRipple className={classes.tabButton} onClick={() => setCurrentTab('preview')}>
           Aperçu
         </Button>
       </Tab>
-
-      <TabFiller />
     </Grid>
   );
 };
