9c0588b14569447b98e9a28a62a9dcf851c9bd36
Nils <nils@epita.fr>
2021-01-24 20:54:52 +0100

    fix(app): fix start script

--- app/start.sh
+++ app/start.sh
@@ -8,11 +8,11 @@ fi
 
 sed -i "s|__ROOT_DIR__|$root|" /etc/nginx/conf.d/default.conf
 
-sed -i "s|__API_URL__|$API_URL|g" "$root"/js/*.js
-sed -i "s|__WEBSITE_URL__|$WEBSITE_URL|g" "$root"/js/*.js
-sed -i "s|__ANALYTICS_URL__|$ANALYTICS_URL|g" "$root"/js/*.js
-sed -i "s|__ANALYTICS_SITE_ID__|$ANALYTICS_SITE_ID|g" "$root"/js/*.js
-sed -i "s|__SENTRY_DSN__|$SENTRY_DSN|g" "$root"/js/*.js
-sed -i "s|__DEBUG__|$DEBUG|g" "$root"/js/*.js
+sed -i "s|__API_URL__|$API_URL|g" "$root"/*.js
+sed -i "s|__WEBSITE_URL__|$WEBSITE_URL|g" "$root"/*.js
+sed -i "s|__ANALYTICS_URL__|$ANALYTICS_URL|g" "$root"/*.js
+sed -i "s|__ANALYTICS_SITE_ID__|$ANALYTICS_SITE_ID|g" "$root"/*.js
+sed -i "s|__SENTRY_DSN__|$SENTRY_DSN|g" "$root"/*.js
+sed -i "s|__DEBUG__|$DEBUG|g" "$root"/*.js
 
 exec nginx -g 'daemon off;'
