228ef0459c855b092027fe73cc7b2fe4be60c034
Bopzor <violaine.ballandin@gmail.com>
2021-02-07 15:25:45 +0100

    fix(integration/liberation): update LIBERATION_REGEXP and getElement to match updated liberation site

--- extension/src/integrations/liberation.ts
+++ extension/src/integrations/liberation.ts
@@ -1,14 +1,14 @@
 import { Integration } from '../integration/IntegrationHost';
 
 export class Liberation implements Integration {
-  static LIBERATION_REGEXP = /liberation\.fr\/([-a-z]+)\/(\d{4}\/\d{2}\/\d{2})\/[-a-z0-9]+_([0-9]+)$/;
+  static LIBERATION_REGEXP = /liberation\.fr(\/[-a-z0-9]+)+\/[-a-z0-9]+_([A-Z0-9]+)/;
 
   name = 'liberation';
   domains = ['www.liberation.fr'];
   type = 'append' as const;
 
   getElement() {
-    return document.getElementsByClassName('article-body')[0].closest('.container-column') as HTMLElement;
+    return document.getElementsByClassName('tag-container')[0] as HTMLElement;
   }
 
   getIdentifier(url: string) {
