69353e45b9d508db977f2fc0af65b8517daebfc7
Nils <nils@epita.fr>
2021-02-18 22:58:57 +0100

    chore(api): allow the fake lag to be configured through an environment variable

--- api/src/common/lag.middleware.ts
+++ api/src/common/lag.middleware.ts
@@ -2,15 +2,14 @@ import { Inject, Injectable, NestMiddleware } from '@nestjs/common';
 
 @Injectable()
 export class LagMiddleware implements NestMiddleware {
-
   constructor(
     @Inject('FAKE_LAG')
     private readonly fakeLag: number,
   ) {}
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  use(req: any, res: any, next: () => void) {
-    setTimeout(next, this.fakeLag);
+  use(req: unknown, res: unknown, next: () => void) {
+    if (this.fakeLag) {
+      setTimeout(next, this.fakeLag);
+    }
   }
-
 }
