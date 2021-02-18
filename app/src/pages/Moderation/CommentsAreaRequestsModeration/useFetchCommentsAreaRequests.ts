f9431de7cd5a8e7315c2aecdd18c7586fcfe8024
Nils <nils@epita.fr>
2021-02-18 18:58:09 +0100

    feat(app): remove all legacy pages and views

--- app/src/pages/Moderation/CommentsAreaRequestsModeration/useFetchCommentsAreaRequests.ts
+++
@@ -1,8 +0,0 @@
-import useAxiosPaginated from 'src/hooks/use-axios-paginated';
-import { CommentsAreaRequest } from 'src/types/CommentsArea';
-
-const useFetchCommentsAreaRequests = () => {
-  return useAxiosPaginated('/api/comments-area/request', undefined, CommentsAreaRequest);
-};
-
-export default useFetchCommentsAreaRequests;
