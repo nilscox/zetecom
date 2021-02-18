240871749273c4b4c7f84b1037b19a49e3b2679c
Nils <nils@epita.fr>
2021-02-19 00:51:26 +0100

    feat(app): add the comment container component

---
+++ app/src/pages/CommentsAreasListPage/CommentsAreaRequest/CommentsAreaRequest.tsx
@@ -0,0 +1,43 @@
+import React, { useRef, useState } from 'react';
+
+import styled from '@emotion/styled';
+
+import CommentsAreaForm, { CommentsAreaFormState } from 'src/components/domain/CommentsAreaForm/CommentAreaForm';
+import Button from 'src/components/elements/Button/Button';
+import Collapse from 'src/components/layout/Collapse/Collapse';
+import Fade from 'src/components/layout/Fade/Fade';
+import { spacing } from 'src/theme';
+
+const Container = styled.div<{ minHeight: number }>`
+  min-height: ${props => props.minHeight}px;
+  margin: ${spacing(4, 0)};
+  position: relative;
+`;
+
+const CommentsAreaRequest: React.FC = () => {
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  const [displayForm, setDisplayForm] = useState(false);
+
+  const handleSubmit = (values: CommentsAreaFormState) => {};
+
+  return (
+    <Container minHeight={buttonRef.current?.clientHeight}>
+      <Fade in={!displayForm}>
+        <Button ref={buttonRef} size="large" onClick={() => setDisplayForm(true)} style={{ position: 'absolute' }}>
+          Ouvrir une zone de commentaires
+        </Button>
+      </Fade>
+
+      <Collapse in={displayForm}>
+        <CommentsAreaForm
+          fieldErrors={{}}
+          clearFieldError={() => {}}
+          onCancel={() => setDisplayForm(false)}
+          onSubmit={handleSubmit}
+        />
+      </Collapse>
+    </Container>
+  );
+};
+
+export default CommentsAreaRequest;
--- app/src/pages/Pages.tsx
+++ app/src/pages/Pages.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import styled from '@emotion/styled';
 import { Route, Switch } from 'react-router';
@@ -6,16 +6,32 @@ import { Route, Switch } from 'react-router';
 import Footer from 'src/components/domain/Footer/Footer';
 import HeaderLogo from 'src/components/domain/HeaderLogo/HeaderLogo';
 import UserMenu from 'src/components/domain/UserMenu/UserMenu';
-import { useUser } from 'src/contexts/user';
+import { useSetUser, useUser } from 'src/contexts/user';
 import useAxios from 'src/hooks/useAxios';
 import { size, spacing } from 'src/theme';
 
 import AuthenticationPage from './AuthenticationPage/AuthenticationPage';
+import CommentsAreaPage from './CommentsAreaPage/CommentsAreaPage';
+import CommentsAreasListPage from './CommentsAreasListPage/CommentsAreasListPage';
 
 const Page = styled.div`
   max-width: 1000px;
   margin: auto;
   margin-top: ${spacing(4)};
+
+  @media (max-width: 1280px) {
+    max-width: 720px;
+  }
+
+  @media (max-width: 768px) {
+    max-width: 600px;
+  }
+
+  @media (max-width: 640px) {
+    max-width: 100%;
+    margin-left: ${spacing(4)};
+    margin-right: ${spacing(4)};
+  }
 `;
 
 const Main = styled.main`
@@ -23,7 +39,15 @@ const Main = styled.main`
 `;
 
 const Pages: React.FC = () => {
-  const [, , logout] = useAxios({ method: 'POST', url: '/api/auth/logout' }, { manual: true });
+  const setUser = useSetUser();
+
+  const [, { status }, logout] = useAxios({ method: 'POST', url: '/api/auth/logout' }, { manual: true });
+
+  useEffect(() => {
+    if (status(204)) {
+      setUser(null);
+    }
+  }, [status, setUser]);
 
   return (
     <Page>
@@ -31,6 +55,8 @@ const Pages: React.FC = () => {
 
       <Main>
         <Switch>
+          <Route path="/" exact component={CommentsAreasListPage} />
+          <Route path="/commentaires/:commentsAreaId" component={CommentsAreaPage} />
           <Route path="/(connexion|inscription|connexion-par-email)" component={AuthenticationPage} />
         </Switch>
       </Main>
