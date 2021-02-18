debfb4d5a4e25c186e448b337a765012b35eac92
Nils <nils@epita.fr>
2021-02-18 18:58:09 +0100

    feat(app): remove all legacy pages and views

--- app/src/contexts/SearchQueryContext.ts
+++
@@ -1,7 +0,0 @@
-import { createContext } from 'react';
-
-const SearchQueryContext = createContext<string | undefined>(undefined);
-
-export default SearchQueryContext;
-export const SearchQueryProvider = SearchQueryContext.Provider;
-export const SearchQueryConsumer = SearchQueryContext.Consumer;
