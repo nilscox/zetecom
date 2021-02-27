d3eb6b594c02ba587ddcfeb2534ee2924fdc8454
Nils <nils@epita.fr>
2021-02-27 17:09:32 +0100

    refactor(app): rewrite the comments areas list container react-query

--- app/src/pages/CommentsAreasListPage/CommentsAreasListPage.tsx
+++ app/src/pages/CommentsAreasListPage/CommentsAreasListPage.tsx
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import axios, { AxiosRequestConfig } from 'axios';
+import { QueryFunction, useQuery } from 'react-query';
+import { useDebounce } from 'use-debounce/lib';
 
 import FiltersBar from 'src/components/domain/FiltersBar/FiltersBar';
 import Box from 'src/components/elements/Box/Box';
 import AsyncContent from 'src/components/layout/AsyncContent/AsyncContent';
-import useAxiosPaginated from 'src/hooks/useAxiosPaginated';
 import CommentsAreaRequest from 'src/pages/CommentsAreasListPage/CommentsAreaRequest/CommentsAreaRequest';
 import { CommentsArea } from 'src/types/CommentsArea';
+import { Paginated } from 'src/types/Paginated';
 
 import CommentsAreasList from './CommentsAreasList/CommentsAreasList';
 
+const fetchCommentsAreas: QueryFunction<Paginated<CommentsArea>> = async ({ queryKey: [, { page, search }] }) => {
+  const params: AxiosRequestConfig['params'] = {};
+
+  if (page !== 1) {
+    params.page = page;
+  }
+
+  if (search !== '') {
+    params.search = search;
+  }
+
+  const response = await axios.get<Paginated<CommentsArea>>('/api/comments-area', { params });
+
+  return response.data;
+};
+
 const CommentsAreasListPage: React.FC = () => {
-  const [commentsAreas, { loading, page, setPage, search, setSearch }] = useAxiosPaginated<CommentsArea>(
-    '/api/comments-area',
+  const [page, setPage] = useState(1);
+  const [search, setSearch] = useState('');
+  const [searchDebounced] = useDebounce(search, 200);
+
+  const { data: commentsAreas, isLoading: loading } = useQuery(
+    ['commentsAreas', { page, search: searchDebounced }],
+    fetchCommentsAreas,
   );
 
   return (
