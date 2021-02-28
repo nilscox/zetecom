8d6e6b202f026bbca9f93eddfdcae7f5341011de
Nils <nils@epita.fr>
2021-02-28 22:32:18 +0100

    feat(website): update the rules and expose the rules history

--- website/src/pages/Rules/index.tsx
+++ website/src/pages/Rules/index.tsx
@@ -1,116 +1,53 @@
-/* eslint-disable max-lines, react/no-unescaped-entities */
-
-import React, { ReactNode } from 'react';
+import queryString from 'query-string';
+import React from 'react';
+import { Redirect, useLocation } from 'react-router-dom';
 
 import Title from 'src/components/Title';
 
+import Rules_0_1 from './versions/RulesV0.1';
+import Rules_0_2, { Changes as Changes_0_2 } from './versions/RulesV0.2';
+
 import './Rules.scss';
-import Link from 'src/components/Link';
-import RouterLink from 'src/components/Link/RouterLink';
 
-const Rule: React.FC<{ rule: ReactNode }> = ({ rule, children }) => (
-  <div className="rule">
-    <h4 className="rule-title"><em>{ rule }</em></h4>
-    <div className="note rule-description">{ children }</div>
-  </div>
-);
+const versions = {
+  '0.1': [Rules_0_1],
+  '0.2': [Rules_0_2, Changes_0_2],
+};
 
-const Example: React.FC<{ example: ReactNode }> = ({ example, children }) => (
-  <div className="example">
-    <div className="example-text note">{ example }</div>
-    <div className="example-description">{ children }</div>
-  </div>
-);
+const currentVersion = '0.2';
+
+type Version = keyof typeof versions;
+
+const isVersion = (version: string): version is Version => {
+  return Object.keys(versions).includes(version);
+};
 
 const Rules: React.FC = () => {
+  const location = useLocation();
+  const query = queryString.parse(location.search);
+
+  if (query.version && !isVersion(query.version as string)) {
+    return <Redirect to="/charte.html" />;
+  }
+
+  const version = query.version as Version;
+  const [Rules, Changes] = versions[version] ?? versions[currentVersion];
+  const previousVersion = version && Object.keys(versions)[Object.keys(versions).indexOf(version) - 1];
+
   return (
     <>
+      <Title id="Charte">La charte de Zétécom{version && <>, version {version}</>}</Title>
 
-      <Title id="Charte">La charte de Zétécom</Title>
+      {version && Changes && (
+        <div className="changes">
+          <hr />
+          Les différences entre les versions {previousVersion} et {version} de la charte sont :
+          <Changes />
+          <hr />
+        </div>
+      )}
 
-      <p>
-        Cette présente charte propose un ensemble de règles qui définissent l'état d'esprit à adopter pour favoriser des échanges constructifs.
-        Tous les utilisateurs inscrits se doivent de la respecter.
-      </p>
-
-      <p>
-        Sur Zétécom, vous êtes tenus de :
-      </p>
-
-      <Rule rule="1. Maintenir les échanges courtois, sans attaque personnelle.">
-        Lorsque vous répondez à un commentaire, notamment si vous n'êtes pas d'accord avec celui-ci, attaquez-vous aux idées et non aux personnes.
-        Dans tous les cas, veillez à garder un ton respectueux, car la colère et l'énervement ne feront qu'entraver votre crédibilité.
-      </Rule>
-
-      <Rule rule="2. Nourrir le débat.">
-        Chaque commentaire doit avoir un sens au sein de la discussion.
-        Cela peut être une source, un argument, un raisonnement logique, une opinion, une question... du moment qu'il apporte une plus-value.
-      </Rule>
-
-      <Rule rule="3. Décrire chaque point de vue objectivement.">
-        Lorsque vous donnez votre opinion sur un sujet, votre but doit être d'expliquer ce que vous pensez et pourquoi vous pensez cela.
-        Le prosélytisme ou toute forme de publicité sont donc proscrits.
-        Si vous le souhaitez, indiquez <RouterLink to="/faq.html#plateforme">en exposant</RouterLink> le <Link href="https://twitter.com/HygieneMentale/status/1230849591534407685">degré de croyance</Link> (ou degré de certitude) de ce que vous avancez.
-      </Rule>
-
-      <Rule rule="4. Apporter les sources nécessaires à la vérification des faits énoncés.">
-        En particulier lorsque vos affirmations sortent de l'ordinaire, il est important d'expliciter leurs sources.
-        De même prenez le temps de vérifier les messages sourcés, car une affirmation sans preuve peut être rejetée sans preuve.
-      </Rule>
-
-      <Rule rule="5. Rédiger en français correct.">
-        Votre message sera toujours mieux reçu s'il est bien écrit et lisible.
-        Veillez à éviter le registre familier, les abréviations, le style "texto", et les fautes d'orthographe autant que possible. Un outil comme l'extension <Link href="https://languagetool.org/fr/#plugins">languagetool</Link> peut s'avérer utile.
-      </Rule>
-
-      <Rule rule="6. Faire preuve de bienveillance.">
-        Quelle que soit la situation, gardez un état d'esprit coopératif.
-        Mettez-vous à la place des personnes qui liront vos commentaires, et montrez-vous capable de <Link href="https://fr.wikipedia.org/wiki/Principe_de_charit%C3%A9">charité interprétative</Link> en lisant ceux des autres utilisateurs.
-      </Rule>
-
-      <p style={{ marginTop: 40 }}>
-        Ces règles sont la base d'une collaboration intelligente entre tous les utilisateurs.
-        Vous êtes toutefois vivement encouragé.e à employer les outils de la pensée critique et de la rhétorique pour mieux comprendre les points de vue et étayer votre discours.
-      </p>
-
-      <p>
-        Enfin, dans un but d'amélioration en continu, ces règles évoluent. Vous êtes vous-même invité.e à proposer des évolutions si vous le souhaitez, nous en discuterons avec d'autres membres de la communauté.
-      </p>
-
-      <p style={{ marginTop: 40 }}>Quelques exemples de commentaires ne respectant pas la charte :</p>
-
-      <div className="examples">
-
-        <Example example={'"En + elle et ariver a la bourre!!!"'}>
-          Le message est mal rédigé et le langage est familier. Préférez "En plus, elle est arrivée en retard !"
-        </Example>
-
-        <Example example={'"Le journaliste est un imbécile."'}>
-          C'est une attaque sur la personne et les propos sont injurieux.
-          Préférez "Le journaliste a écrit que [...], il n'a donc pas compris."
-        </Example>
-
-      </div>
-
-      <div className="examples">
-
-        <Example example={'"Il ne faut pas avoir peur de l\'énergie nucléaire."'}>
-          Le point de vue n'est pas objectif.
-          Préférez "Selon cette source, l'énergie nucléaire n'est pas à craindre."
-        </Example>
-
-        <Example example={'"Je trouve que vous avez très bien résumé la situation."'}>
-          Le message n'apporte aucune plus-value. Préférez annoter le commentaire d'un "je suis d'accord".
-        </Example>
-
-      </div>
-
-      {/*
-        <p>
-          Pour plus d'exemples s'appliquant aussi à cette charte, faites un tour sur <Link href="https://www.lemonde.fr/actualite-medias/article/2019/05/21/l-espace-des-contributions-du-monde-en-10-questions_5465155_3236.html">la partie 2 de l'espace des contribution du Monde</Link>.
-        </p>
-      */}
-
+      <Rules />
     </>
   );
 };
---
+++ website/src/pages/Rules/versions/RulesV0.2.tsx
@@ -0,0 +1,115 @@
+/* eslint-disable max-lines, react/no-unescaped-entities */
+
+import React, { ReactNode } from 'react';
+
+import Link from 'src/components/Link';
+import RouterLink from 'src/components/Link/RouterLink';
+
+const Rule: React.FC<{ rule: ReactNode }> = ({ rule, children }) => (
+  <div className="rule">
+    <h4 className="rule-title">
+      <em>{rule}</em>
+    </h4>
+    <div className="note rule-description">{children}</div>
+  </div>
+);
+
+const Example: React.FC<{ example: ReactNode }> = ({ example, children }) => (
+  <div className="example">
+    <div className="example-text note">{example}</div>
+    <div className="example-description">{children}</div>
+  </div>
+);
+
+export const Changes: React.FC = () => {
+  return (
+    <ul>
+      <li>Ajout de la règle "Lire les messages en laissant de côté ses aprioris."</li>
+      <li>Ajout de la règle "Expliquer les raisons qui justifient un point de vue."</li>
+      <li>Ajout de la règle "Préciser le degré de certitude dans les affirmations énoncées."</li>
+      <li>Suppression de la règle "Nourrir le débat."</li>
+      <li>Suppression de la règle "Décrire chaque point de vue objectivement."</li>
+      <li>Reformulation de toutes les autres règles</li>
+      <li>Suppression de l'introduction et reformulation de la conclusion</li>
+      <li>Suppression des exemples devenus non pertinents</li>
+    </ul>
+  );
+};
+
+const Rules: React.FC = () => {
+  return (
+    <>
+      <Rule rule="1. Lire les messages en laissant de côté ses aprioris.">
+        Les zones de commentaires permettent d'exprimer publiquement ses opinions, certaines ne s'accorderont donc
+        naturellement pas avec les vôtres. Cherchez à comprendre la manière de réfléchir de l'auteur, en faisant preuve
+        de <Link href="https://fr.wikipedia.org/wiki/Principe_de_charit%C3%A9">charité interprétative</Link>. Le temps
+        de la lecture, essayez de vous abstraire de vos propres opinions, de suspendre votre jugement.
+      </Rule>
+
+      <Rule rule="2. Maintenir les échanges courtois, sans attaque personnelle.">
+        Attaquez-vous aux idées et non aux personnes lorsque vous participez aux échanges, tout en gardant un ton
+        respectueux. Cherchez les formulations qui donneront envie à vos interlocuteurs de vous répondre, surtout s'ils
+        sont susceptibles d'avoir un avis différent.
+      </Rule>
+
+      <Rule rule="3. Expliquer les raisons qui justifient un point de vue.">
+        Lorsque vous prenez conscience des croyances et certitudes d'autres personnes, vous serez certainement
+        intéressé.e de comprendre les raisons qui les justifient. Vous êtes donc à votre tour invité.e à préciser vos
+        raisons de croire ce que vous estimez être vrai ainsi que votre chemin de pensée, votre méthode.
+      </Rule>
+
+      <Rule rule="4. Préciser le degré de certitude dans les affirmations énoncées.">
+        Pour vous assurer d'être bien compris, il peut vous être utile d'exprimer à quel point vous êtes sûr.e des
+        affirmations clés que vous avancez. Zétécom{' '}
+        <RouterLink to="/faq.html#plateforme">permet d'annoter une phrase</RouterLink> de ce degré de croyance, avec{' '}
+        <Link href="https://twitter.com/HygieneMentale/status/1230849591534407685">une notation en exposant</Link>,
+        comme ceci<sup>80</sup>.
+      </Rule>
+
+      <Rule rule="5. Apporter les sources nécessaires à la vérification des faits avancés.">
+        En particulier lorsque vos affirmations sortent de l'ordinaire, il est important d'expliciter leurs sources. De
+        même prenez le temps de vérifier les messages sourcés, et de les remettre en question si nécessaire.
+      </Rule>
+
+      <Rule rule="6. Rédiger en français correct.">
+        Votre message sera toujours mieux perçu s'il est bien écrit et lisible. Évitez le registre familier, les
+        abréviations et le style "texto". Une extension comment{' '}
+        <Link href="https://languagetool.org/fr/#plugins">languagetool</Link> vous permettra même de corriger les fautes
+        d'orthographe directement pendant la rédaction d'un message.
+      </Rule>
+
+      <Rule rule="7. Faire preuve de bienveillance.">
+        Lorsque vous répondez à un commentaire, mettez-vous à la place des personnes qui liront le vôtre, et gardez un
+        état d'esprit coopératif. N'hésitez pas à reformuler les propos des autres intervenants pour vous assurer d'en
+        avoir compris le fond.
+      </Rule>
+
+      <p style={{ marginTop: 40 }}>
+        Ces règles peuvent être applicables ou non et de manière différentes selon les situations. Elles ne sont pas
+        absolues, mais définissent plutôt un état d'esprit général à adopter dans le but de favoriser des échanges
+        constructifs. Vous êtes toutefois vivement encouragé.e à employer les outils de la pensée critique et de la
+        rhétorique pour mieux comprendre chaque point de vue et étayer votre discours.
+      </p>
+
+      <p>
+        Enfin, dans un but d'amélioration en continu, ces règles évoluent. Vous êtes vous-même invité.e à proposer des
+        évolutions si vous le souhaitez, nous en discuterons avec d'autres membres de la communauté.
+      </p>
+
+      <p style={{ marginTop: 40 }}>Quelques exemples de commentaires ne respectant pas la charte :</p>
+
+      <div className="examples">
+        <Example example={'"En + elle et ariver a la bourre!!!"'}>
+          Le message est mal rédigé et le langage est familier. Préférez "En plus, elle est arrivée en retard !"
+        </Example>
+
+        <Example example={'"Le journaliste est un imbécile."'}>
+          C'est une attaque sur la personne et les propos sont injurieux. Préférez "Le journaliste a écrit que [...], il
+          n'a donc pas compris."
+        </Example>
+      </div>
+    </>
+  );
+};
+
+export default Rules;
