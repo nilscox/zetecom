c86dda8d339e93ae2881517893951bbcf6b2cbf9
Nils <nils@epita.fr>
2021-03-03 20:06:38 +0100

    test(integration): add a failing test for liberation

--- extension/src/integrations/__tests__/liberation.test.ts
+++ extension/src/integrations/__tests__/liberation.test.ts
@@ -3,9 +3,19 @@ import { Liberation } from '../liberation';
 describe('liberation', () => {
   it('url 1', () => {
     const liberation = new Liberation();
-    const url = 'https://www.liberation.fr/france/2020/11/21/loi-securite-globale-venir-ici-avec-la-peur-au-ventre-c-est-triste-pour-la-democratie_1806336';
+    const url =
+      'https://www.liberation.fr/france/2020/11/21/loi-securite-globale-venir-ici-avec-la-peur-au-ventre-c-est-triste-pour-la-democratie_1806336';
     const identifier = liberation.getIdentifier(url);
 
     expect(identifier).toEqual('liberation:france:2020-11-21:1806336');
+  });
+
+  it('url 2', () => {
+    const liberation = new Liberation();
+    const url =
+      'https://www.liberation.fr/societe/a-chalon-sur-saone-des-forains-sous-perfusion-reclament-des-reponses-concretes-20210303_JCH6CMACYBGRXAFR7YWBCUZ4IE/';
+    const identifier = liberation.getIdentifier(url);
+
+    expect(identifier).toEqual('liberation:societe:2021-03-03:JCH6CMACYBGRXAFR7YWBCUZ4IE');
   });
 });
