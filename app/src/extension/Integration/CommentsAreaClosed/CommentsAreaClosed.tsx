bcd7afcaf78e75c522b46a9e0bea1a4fc275d169
Nils <nils@epita.fr>
2021-03-05 00:10:04 +0100

    fix(integration): add a loading state indicator to the comments area request button

--- app/src/extension/Integration/CommentsAreaClosed/CommentsAreaClosed.tsx
+++ app/src/extension/Integration/CommentsAreaClosed/CommentsAreaClosed.tsx
@@ -1,24 +1,50 @@
 import React from 'react';
 
 import styled from '@emotion/styled';
+import axios from 'axios';
+import { useMutation } from 'react-query';
 
 import Button from 'src/components/elements/Button/Button';
 import Fallback from 'src/components/layout/Fallback/Fallback';
-import { fontSize, spacing } from 'src/theme';
+import { useTrackEvent } from 'src/contexts/trackingContext';
 import { useUser } from 'src/contexts/userContext';
+import track from 'src/domain/track';
+import useQueryString from 'src/hooks/use-query-string';
+import { fontSize, spacing } from 'src/theme';
+
+const requestCommentsArea = async (identifier: string, informationUrl: string) => {
+  await axios.post('/api/comments-area/request', { identifier, informationUrl });
+};
+
+const useRequestCommetsArea = (identifier: string, informationUrl: string) => {
+  const trackEvent = useTrackEvent();
+
+  const { mutate, isLoading: requesting, isSuccess: requested } = useMutation(
+    () => requestCommentsArea(identifier, informationUrl),
+    {
+      onSuccess: () => {
+        trackEvent(track.commentsAreaRequested('Integration'));
+      },
+    },
+  );
+
+  return [mutate, { requesting, requested }] as const;
+};
 
 const CommentsAreaClosedFallback = styled.div`
   font-size: ${fontSize('xlarge')};
   margin-bottom: ${spacing(2)};
 `;
 
-type CommentsAreaClosed = {
-  requested: boolean;
-  onRequest: () => void;
+type CommentsAreaClosedProps = {
+  identifier: string;
 };
 
-const CommentsAreaClosed: React.FC<CommentsAreaClosed> = ({ requested, onRequest }) => {
+const CommentsAreaClosed: React.FC<CommentsAreaClosedProps> = ({ identifier }) => {
+  const { pageUrl: informationUrl } = useQueryString();
   const user = useUser();
+
+  const [onRequest, { requesting, requested }] = useRequestCommetsArea(identifier, informationUrl as string);
 
   return (
     <Fallback
@@ -30,7 +56,7 @@ const CommentsAreaClosed: React.FC<CommentsAreaClosed> = ({ requested, onRequest
           </CommentsAreaClosedFallback>
 
           {user && !requested && (
-            <Button size="large" onClick={onRequest}>
+            <Button size="large" loading={requesting} onClick={() => onRequest()}>
               Demander l'ouverture
             </Button>
           )}
--- app/src/extension/Integration/Integration.tsx
+++ app/src/extension/Integration/Integration.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 
 import styled from '@emotion/styled';
-import axios from 'axios';
-import { useMutation } from 'react-query';
 import { RouteComponentProps, useLocation } from 'react-router';
 
 import HeaderLogo from 'src/components/domain/HeaderLogo/HeaderLogo';
@@ -11,25 +9,8 @@ import { useTracker, useTrackEvent } from 'src/contexts/trackingContext';
 import track from 'src/domain/track';
 import useIFrameMessages from 'src/extension/hooks/useIFrameMessages';
 import CommentsAreaClosed from 'src/extension/Integration/CommentsAreaClosed/CommentsAreaClosed';
-import useQueryString from 'src/hooks/use-query-string';
 import { color, spacing } from 'src/theme';
 import { CommentsArea } from 'src/types/CommentsArea';
-
-const requestCommentsArea = async (identifier: string, informationUrl: string) => {
-  await axios.post('/api/comments-area/request', { identifier, informationUrl });
-};
-
-const useRequestCommetsArea = (identifier: string, informationUrl: string) => {
-  const trackEvent = useTrackEvent();
-
-  const { mutate, isSuccess: requested } = useMutation(() => requestCommentsArea(identifier, informationUrl), {
-    onSuccess: () => {
-      trackEvent(track.commentsAreaRequested('Integration'));
-    },
-  });
-
-  return [mutate, { requested }] as const;
-};
 
 const Container = styled.div`
   border: 1px solid ${color('border')};
@@ -38,9 +19,6 @@ const Container = styled.div`
 
 const Integration: React.FC<RouteComponentProps<{ identifier: string }>> = ({ match }) => {
   const { identifier } = match.params;
-  const { pageUrl: informationUrl } = useQueryString();
-
-  const [onRequest, { requested }] = useRequestCommetsArea(identifier, informationUrl as string);
 
   const [sendMessage] = useIFrameMessages();
 
@@ -60,7 +38,7 @@ const Integration: React.FC<RouteComponentProps<{ identifier: string }>> = ({ ma
       <HeaderLogo small link={false} />
       <CommentsAreaContainer
         commentsAreaIdentifier={identifier}
-        notFoundFallback={<CommentsAreaClosed requested={requested} onRequest={onRequest} />}
+        notFoundFallback={<CommentsAreaClosed identifier={identifier} />}
         onCommentsAreaLoaded={handleCommentsAreaLoaded}
       />
     </Container>
