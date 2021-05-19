23d0562a8ea031fc9264652b399e72ee4ffb631d
Nils <nils@epita.fr>
2021-05-19 12:44:25 +0200

    chore(api): fix modules dependencies

--- api/src/modules/comments-area/comments-area-request/comments-area-request.module.ts
+++ api/src/modules/comments-area/comments-area-request/comments-area-request.module.ts
@@ -1,8 +1,6 @@
-import { forwardRef, Module } from '@nestjs/common';
-import { CqrsModule } from '@nestjs/cqrs';
+import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { CommentsAreaModule } from 'src/modules/comments-area/comments-area.module';
 import { UserModule } from 'src/modules/user/user.module';
 
 import { CommentsAreaRequestController } from './comments-area-request.controller';
@@ -10,12 +8,7 @@ import { CommentsAreaRequest } from './comments-area-request.entity';
 import { CommentsAreaRequestService } from './comments-area-request.service';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([CommentsAreaRequest]),
-    CqrsModule,
-    UserModule,
-    forwardRef(() => CommentsAreaModule),
-  ],
+  imports: [TypeOrmModule.forFeature([CommentsAreaRequest]), UserModule],
   controllers: [CommentsAreaRequestController],
   providers: [CommentsAreaRequestService],
   exports: [CommentsAreaRequestService],
--- api/src/modules/comments-area/comments-area.module.ts
+++ api/src/modules/comments-area/comments-area.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { CqrsModule } from '@nestjs/cqrs';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { UserModule } from 'src/modules/user/user.module';
@@ -8,10 +7,16 @@ import { CommentsAreaController } from './comments-area.controller';
 import { CommentsAreaRepository } from './comments-area.repository';
 import { CommentsAreaService } from './comments-area.service';
 import { CommentsAreaIntegrationModule } from './comments-area-integration/comments-area-integration.module';
+import { CommentsAreaRequestModule } from './comments-area-request/comments-area-request.module';
 import { PopulateCommentsArea } from './populate-comments-area.interceptor';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CommentsAreaRepository]), CqrsModule, UserModule, CommentsAreaIntegrationModule],
+  imports: [
+    TypeOrmModule.forFeature([CommentsAreaRepository]),
+    UserModule,
+    CommentsAreaRequestModule,
+    CommentsAreaIntegrationModule,
+  ],
   controllers: [CommentsAreaController],
   providers: [CommentsAreaService, PopulateCommentsArea],
   exports: [TypeOrmModule, CommentsAreaService, PopulateCommentsArea],
