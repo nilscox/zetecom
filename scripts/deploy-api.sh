ac3f81662f05ac0a641aed7addb900d46080cc91
Nils <nils@epita.fr>
2021-11-17 22:52:55 +0100

    ci: deploy the api automatically

--- scripts/deploy-api.sh
+++ scripts/deploy-api.sh
@@ -1,100 +1,117 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-source $(dirname "$0")/functions.sh
-environment=$1
+script_dir=$(cd "$(dirname "$BASH_SOURCE[0]}")" && pwd)
 
-shift
-command="$1"
+env="local"
+image="nilscox/zetecom-api:$(git rev-parse HEAD)"
+port="4242"
 
-prepare_deployment() {
-  if [ "$environment" != 'production' ] && [ "$environment" != 'staging' ] && [ "$environment" != 'local' ]; then
-    echo 'usage: deploy-api.sh [staging|production]' >&2
+docker_host="https://localhost:2375"
+docker_auth=""
+base_dir=""
+
+while [ -n "$1" ]; do
+  case "$1" in
+    --env)
+      env="$2"
+      shift; shift;;
+
+    --image)
+      image="$2"
+      shift; shift;;
+
+    --port)
+      port="$2"
+      shift; shift;;
+
+    --docker-host)
+      docker_host="$2"
+      shift; shift;;
+
+    --docker-auth)
+      docker_auth="$2"
+      shift; shift;;
+
+    --base-dir)
+      base_dir="$2"
+      shift; shift;;
+
+    *)
+      echo "warning: unkonwn argument \"$1\"" >&2
+      shift;;
+  esac
+done
+
+echo "base_dir=$base_dir"
+
+if [ -n "$base_dir" ]; then
+  base_dir=$(realpath "$script_dir/../data/zetecom-local")
+fi
+
+container="zc-api-$env"
+network="zc-network-$env"
+
+dkr() {
+  path="$1"; shift
+
+  curl -v \
+    "$docker_host$path" \
+    -u "$docker_auth" \
+    --header 'Content-Type: application/json' \
+    "$@"
+
+  if [ "$?" -ne 0 ]; then
     exit 1
   fi
-
-  echo "Environment: $environment"
 }
 
-setup_environment () {
-  load_env deploy.env
-  load_env api.env
-  echo
+dkr "/containers/$container?force=true" --request DELETE
 
-  echo "Environment variables:"
-  echo_env api.env
-  echo
+dkr "/containers/create?name=$container" --data '{
+  "Image": "'"$image"'",
+  "ExposedPorts": {
+    "80/tcp": {}
+  },
+  "HostConfig": {
+    "NetworkMode": "'"$network"'",
+    "PortBindings": {
+      "80/tcp": [{ "HostIp": "127.0.0.1", "HostPort": "'"$port"'" }]
+    },
+    "Binds": [
+      "'"$base_dir"'/avatars:/app/avatars:rw"
+    ]
+  },
+  "Env": [
+    "LISTEN_IP=0.0.0.0",
+    "LISTEN_PORT=80",
+    "LOG_LEVEL='"$LOG_LEVEL"'",
+    "TRUST_PROXY='"$TRUST_PROXY"'",
+    "REFLECT_ORIGIN='"$REFLECT_ORIGIN"'",
+    "ADMIN_USER='"$ADMIN_USER"'",
+    "APP_URL='"$APP_URL"'",
+    "WEBSITE_URL='"$WEBSITE_URL"'",
+    "DB_HOST='"$DB_HOST"'",
+    "DB_PORT='"$DB_PORT"'",
+    "DB_USER='"$DB_USER"'",
+    "DB_PASS='"$DB_PASS"'",
+    "DB_NAME='"$DB_NAME"'",
+    "DB_ENTITIES='"$DB_ENTITIES"'",
+    "DB_MIGRATIONS='"$DB_MIGRATIONS"'",
+    "DB_SEEDS='"$DB_SEEDS"'",
+    "REDIS_HOST='"$REDIS_HOST"'",
+    "REDIS_PORT='"$REDIS_PORT"'",
+    "EMAIL_HOST='"$EMAIL_HOST"'",
+    "EMAIL_PORT='"$EMAIL_PORT"'",
+    "EMAIL_USER='"$EMAIL_USER"'",
+    "EMAIL_PASSWORD='"$EMAIL_PASSWORD"'",
+    "EMAIL_SECURE='"$EMAIL_SECURE"'",
+    "EMAIL_BYPASS='"$EMAIL_BYPASS"'",
+    "EMAIL_ACCOUNT_VERIFICATION='"$EMAIL_ACCOUNT_VERIFICATION"'",
+    "EMAIL_FROM='"$EMAIL_FROM"'",
+    "SESSION_SECRET='"$SESSION_SECRET"'",
+    "SECURE_COOKIE='"$SECURE_COOKIE"'",
+    "USER_AVATAR_DESTINATION='"$USER_AVATAR_DESTINATION"'"
+  ]
+}'
 
-  echo "Deployment variables:"
-  echo_vars deploy_user deploy_host deploy_key base_dir api_image api_port
-
-  if [ -n "$deploy_key" ]; then
-    execute ssh-add "$deploy_key"
-  fi
-}
-
-cleanup_environment() {
-  if [ -n "$deploy_key" ]; then
-    execute ssh-add -d "$deploy_key"
-  fi
-}
-
-deploy_api() {
-  execute ssh "$deploy_user@$deploy_host" docker pull "$api_image" || true
-  ssh "$deploy_user@$deploy_host" docker rm -f "zc-api-$environment" || true
-
-  ssh_args=""
-  docker_args="-dt"
-
-  if [ -n "$command" ]; then
-    ssh_args="-tt"
-    docker_args="-it"
-  fi
-
-  execute ssh "$ssh_args" "$deploy_user@$deploy_host" docker run "$docker_args" \
-    --name "zc-api-$environment" \
-    --network "zc-network-$environment" \
-    --volume "$base_dir/avatars:/app/avatars:rw" \
-    -p "127.0.0.1:$api_port:80" \
-    --env LISTEN_IP='0.0.0.0' \
-    --env LISTEN_PORT='80' \
-    --env $(sshenv LOG_LEVEL) \
-    --env $(sshenv TRUST_PROXY) \
-    --env $(sshenv REFLECT_ORIGIN) \
-    --env $(sshenv ADMIN_USER) \
-    --env $(sshenv APP_URL) \
-    --env $(sshenv WEBSITE_URL) \
-    --env $(sshenv DB_HOST) \
-    --env $(sshenv DB_PORT) \
-    --env $(sshenv DB_USER) \
-    --env $(sshenv DB_PASS) \
-    --env $(sshenv DB_NAME) \
-    --env $(sshenv DB_ENTITIES) \
-    --env $(sshenv DB_MIGRATIONS) \
-    --env $(sshenv DB_SEEDS) \
-    --env $(sshenv REDIS_HOST) \
-    --env $(sshenv REDIS_PORT) \
-    --env $(sshenv EMAIL_HOST) \
-    --env $(sshenv EMAIL_PORT) \
-    --env $(sshenv EMAIL_USER) \
-    --env $(sshenv EMAIL_PASSWORD) \
-    --env $(sshenv EMAIL_SECURE) \
-    --env $(sshenv EMAIL_BYPASS) \
-    --env $(sshenv EMAIL_ACCOUNT_VERIFICATION) \
-    --env $(sshenv EMAIL_FROM) \
-    --env $(sshenv SESSION_SECRET) \
-    --env $(sshenv SECURE_COOKIE) \
-    --env $(sshenv USER_AVATAR_DESTINATION) \
-    "$api_image" \
-    "$command"
-}
-
-main() {
-  echo_title "Deploy API"
-  step "Prepare deployment" prepare_deployment
-  step "Setup environment" setup_environment
-  step "Deploy API" deploy_api
-  step "Cleanup environment" cleanup_environment
-  echo "Deployment success"
-}
-
-main
+dkr "/containers/$container/start" --request POST
